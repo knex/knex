745d36c295fd4733b08a6634ba77a1ac8625fae6
Tim Griesser <tgriesser10@gmail.com>
2013-08-20 00:50:04 -0400

    prep for 0.2.2

--- knex.js
+++ knex.js
@@ -1,4 +1,4 @@
-//     Knex.js  0.2.1
+//     Knex.js  0.2.2
 //
 //     (c) 2013 Tim Griesser
 //     Knex may be freely distributed under the MIT license.
@@ -23,7 +23,7 @@
   };
 
   // Keep in sync with package.json
-  Knex.VERSION = '0.2.1';
+  Knex.VERSION = '0.2.2';
 
   // Methods common to both the `Grammar` and `SchemaGrammar` interfaces,
   // used to generate the sql in one form or another.
