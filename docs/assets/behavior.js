b108d6fd5b5565e7adebbd5df3dd5d5dcd81c095
Tim Griesser <tgriesser10@gmail.com>
2013-09-27 21:50:46 -0400

    officially add and document column method

--- docs/assets/behavior.js
+++ docs/assets/behavior.js
@@ -545,8 +545,8 @@ f.event={add:function(a,c,d,e,g){var h,i,j,k,l,m,n,o,p,q,r,s;if(!(a.nodeType===3
           "type": "heading",
           "data": {
             "level": 2,
-            "title": "Knex.js  0.4.6",
-            "slug": "knexjs-046"
+            "title": "Knex.js  0.4.7",
+            "slug": "knexjs-047"
           },
           "depth": 2
         }
