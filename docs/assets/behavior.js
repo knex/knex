dfd7f7fa6987a3388657ee6633e03c077e983dcc
Tim Griesser <tgriesser10@gmail.com>
2013-10-29 13:58:19 -0400

    re-doc for 0.4.12 release

--- docs/assets/behavior.js
+++ docs/assets/behavior.js
@@ -545,8 +545,8 @@ f.event={add:function(a,c,d,e,g){var h,i,j,k,l,m,n,o,p,q,r,s;if(!(a.nodeType===3
           "type": "heading",
           "data": {
             "level": 2,
-            "title": "Knex.js  0.4.11",
-            "slug": "knexjs-0411"
+            "title": "Knex.js  0.4.12",
+            "slug": "knexjs-0412"
           },
           "depth": 2
         }
