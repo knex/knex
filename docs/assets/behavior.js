373868cfbccd410906053ffabafd20391bad88f2
Tim Griesser <tgriesser10@gmail.com>
2013-12-18 17:27:16 -0500

    docs update for 0.5.2

--- docs/assets/behavior.js
+++ docs/assets/behavior.js
@@ -545,8 +545,8 @@ f.event={add:function(a,c,d,e,g){var h,i,j,k,l,m,n,o,p,q,r,s;if(!(a.nodeType===3
           "type": "heading",
           "data": {
             "level": 2,
-            "title": "Knex.js  0.5.1",
-            "slug": "knexjs-051"
+            "title": "Knex.js  0.5.2",
+            "slug": "knexjs-052"
           },
           "depth": 2
         }
