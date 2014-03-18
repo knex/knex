c1376817ce8a1208f588df4f57b29862da8a31c7
Tim Griesser <tgriesser10@gmail.com>
2014-03-18 15:36:40 -0400

    docs prep for release

--- docs/assets/behavior.js
+++ docs/assets/behavior.js
@@ -621,8 +621,8 @@ f.event={add:function(a,c,d,e,g){var h,i,j,k,l,m,n,o,p,q,r,s;if(!(a.nodeType===3
           "type": "heading",
           "data": {
             "level": 2,
-            "title": "Knex.js  0.5.8",
-            "slug": "knexjs-058"
+            "title": "Knex.js  0.5.9",
+            "slug": "knexjs-059"
           },
           "depth": 2
         }
