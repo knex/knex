5bac6747a58e60a0c2ed1f81a3f945eeb2309f5b
Tim Griesser <tgriesser10@gmail.com>
2014-04-16 11:38:03 -0400

    prepInsert is now working.

--- test/index.js
+++ test/index.js
@@ -21,10 +21,25 @@ var mysql    = knex({client: 'mysql'});
 var sqlite3  = knex({client: 'sqlite3'});
 var postgres = knex({client: 'postgres'});
 
-require('./unit/schema/postgresql')(postgres.client);
-require('./unit/schema/sqlite3')(sqlite3.client);
-require('./unit/schema/mysql')(mysql.client);
-require('./unit/query/builder')(postgres.client, mysql.client, sqlite3.client);
+describe('Unit tests', function() {
+  require('./unit/schema/postgresql')(postgres.client);
+  require('./unit/schema/sqlite3')(sqlite3.client);
+  require('./unit/schema/mysql')(mysql.client);
+  require('./unit/query/builder')(postgres.client, mysql.client, sqlite3.client);
+
+  it('provides a knex.utils.prepInsert for normalizing js objects', function() {
+    var data = [{a: 1}, {b: 2}, {a: 2, c: 3}];
+    var dataPrepped = mysql.utils.prepInsert(data);
+    assert.deepEqual(dataPrepped, [
+      {a: 1, b: void 0, c: void 0},
+      {a: void 0, b: 2, c: void 0},
+      {a: 2, b: void 0, c: 3}
+    ]);
+    assert.notEqual(data, dataPrepped);
+  });
+
+});
+
 
 // Integration Tests
 describe('Integration Tests', function() {
