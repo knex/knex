98765c38c7e43a4bd533903d04679ef4c613126d
Tim Griesser <tgriesser10@gmail.com>
2014-05-08 18:04:55 -0400

    adding tests for change in #265

--- test/integration/builder/transaction.js
+++ test/integration/builder/transaction.js
@@ -22,7 +22,7 @@ module.exports = function(knex) {
           .insert({
             first_name: 'Transacting',
             last_name: 'User',
-            email:'transaction-test@example.com',
+            email:'transaction-test1@example.com',
             logins: 1,
             about: 'Lorem ipsum Dolore labore incididunt enim.',
             created_at: new Date(),
@@ -48,9 +48,7 @@ module.exports = function(knex) {
     it('should be able to rollback transactions', function() {
       var id = null;
       var err = new Error('error message');
-
       return knex.transaction(function(t) {
-
         knex('accounts')
           .transacting(t)
           .returning('id')
@@ -63,31 +61,82 @@ module.exports = function(knex) {
             created_at: new Date(),
             updated_at: new Date()
           }).then(function(resp) {
-
             return knex('test_table_two').transacting(t).insert({
               account_id: (id = resp[0]),
               details: '',
               status: 1
             });
-
           }).then(function() {
-
             t.rollback(err);
-
           });
-
-      }).otherwise(function(msg) {
-
+      }).catch(function(msg) {
         expect(msg).to.equal(err);
-
         return knex('accounts').where('id', id).select('first_name');
-
       }).then(function(resp) {
-
         expect(resp).to.be.empty;
-
       });
+    });
 
+    it('should be able to commit transactions with a resolved trx query', function() {
+
+      var id = null;
+      return knex.transaction(function(trx) {
+        return trx('accounts')
+          .returning('id')
+          .insert({
+            first_name: 'Transacting',
+            last_name: 'User',
+            email:'transaction-test3@example.com',
+            logins: 1,
+            about: 'Lorem ipsum Dolore labore incididunt enim.',
+            created_at: new Date(),
+            updated_at: new Date()
+          }).then(function(resp) {
+            return trx('test_table_two').insert({
+              account_id: (id = resp[0]),
+              details: '',
+              status: 1
+            });
+          }).then(function() {
+            return 'Hello World';
+          });
+      }).then(function(commitMessage) {
+        expect(commitMessage).to.equal('Hello World');
+        return knex('accounts').where('id', id).select('first_name');
+      }).then(function(resp) {
+        expect(resp).to.have.length(1);
+      });
+    });
+
+    it('should be able to rollback transactions with rejected trx query', function() {
+      var id = null;
+      var err = new Error('error message');
+      return knex.transaction(function(trx) {
+        return trx('accounts')
+          .returning('id')
+          .insert({
+            first_name: 'Transacting',
+            last_name: 'User2',
+            email:'transaction-test4@example.com',
+            logins: 1,
+            about: 'Lorem ipsum Dolore labore incididunt enim.',
+            created_at: new Date(),
+            updated_at: new Date()
+          }).then(function(resp) {
+            return trx.insert({
+              account_id: (id = resp[0]),
+              details: '',
+              status: 1
+            }).into('test_table_two');
+          }).then(function() {
+            throw err;
+          });
+      }).catch(function(msg) {
+        expect(msg).to.equal(err);
+        return knex('accounts').where('id', id).select('first_name');
+      }).then(function(resp) {
+        expect(resp).to.be.empty;
+      });
     });
 
   });
