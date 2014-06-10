16f4f29dc8d66a93892747603520063c019e3c72
Tim Griesser <tgriesser10@gmail.com>
2014-06-09 20:26:05 -0400

    release 0.6.8

--- knex.js
+++ knex.js
@@ -1,4 +1,4 @@
-// Knex.js  0.6.7
+// Knex.js  0.6.8
 // --------------
 
 //     (c) 2014 Tim Griesser
@@ -80,7 +80,7 @@ Knex.initialize = function(config) {
 
   // The `__knex__` is used if you need to duck-type check whether this
   // is a knex builder, without a full on `instanceof` check.
-  knex.VERSION = knex.__knex__  = '0.6.7';
+  knex.VERSION = knex.__knex__  = '0.6.8';
   knex.raw = function(sql, bindings) {
     var raw = new client.Raw(sql, bindings);
     raw.on('query', function(data) {
