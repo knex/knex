222ac82a5af002e4b53394f6b936ca8d19f5bb86
Tim Griesser <tgriesser10@gmail.com>
2014-07-10 12:34:44 -0400

    release 0.6.22

--- browser/knex.js
+++ browser/knex.js
@@ -1,5 +1,5 @@
 !function(e){if("object"==typeof exports&&"undefined"!=typeof module)module.exports=e();else if("function"==typeof define&&define.amd)define([],e);else{var f;"undefined"!=typeof window?f=window:"undefined"!=typeof global?f=global:"undefined"!=typeof self&&(f=self),f.Knex=e()}}(function(){var define,module,exports;return (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);throw new Error("Cannot find module '"+o+"'")}var f=n[o]={exports:{}};t[o][0].call(f.exports,function(e){var n=t[o][1][e];return s(n?n:e)},f,f.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(_dereq_,module,exports){
-// Knex.js  0.6.21
+// Knex.js  0.6.22
 // --------------
 
 //     (c) 2014 Tim Griesser
@@ -1708,13 +1708,15 @@ function Runner_PG() {
 inherits(Runner_PG, Runner);
 
 var PGQueryStream;
-Runner_PG.prototype._stream = Promise.method(function(sql, stream, options) {
+Runner_PG.prototype._stream = Promise.method(function(obj, stream, options) {
   PGQueryStream = PGQueryStream || _dereq_('pg-query-stream');
   var runner = this;
+  var sql = obj.sql = utils.pgBindings(obj.sql);
+  if (this.isDebugging()) this.debug(obj);
   return new Promise(function(resolver, rejecter) {
     stream.on('error', rejecter);
     stream.on('end', resolver);
-    runner.connection.query(new PGQueryStream(sql.sql, sql.bindings, options)).pipe(stream);
+    runner.connection.query(new PGQueryStream(sql, obj.bindings, options)).pipe(stream);
   });
 });
 
