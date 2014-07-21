690326ea330f09546543ca6574a4ac1f6c2db155
Noah Santorello <nsantorello@gmail.com>
2014-07-21 18:38:40 -0400

    Adding integration tests for seeds

---
+++ test/integration/seed/index.js
@@ -0,0 +1,42 @@
+var equal    = require('assert').equal;
+var path     = require('path');
+var rimraf   = require('rimraf');
+var Promise  = require('../../../lib/promise');
+
+module.exports = function(knex) {
+
+  require('rimraf').sync(path.join(__dirname, './seed'));
+
+  describe('knex.seed', function () {
+
+    describe('knex.seed.make', function() {
+      it('should create a new seed file with the make method', function () {
+        return knex.seed.make('test').then(function (name) {
+          expect(name).to.equal('test');
+        });
+      });
+    });
+
+    describe('knex.seed.run', function() {
+      it('should run all seed files in the configured seed directory', function() {
+        return knex.seed.run({directory: __dirname + '/test'}).then(function(data) {
+          expect(data[0].name).to.equal('seed1.js');
+          expect(data[1].name).to.equal('seed2.js');
+        });
+      });
+
+      it('should raise error if exports.seed function is not defined in seed file', function() {
+        return knex.seed.run({directory: __dirname + '/test'}).then(function(data) {
+          expect(data[0].name).to.equal('seed1.js');
+          expect(data[1].name).to.equal('seed2.js');
+        });
+      });
+    });
+
+    after(function() {
+      rimraf.sync(path.join(__dirname, './seed'));
+    });
+
+  });
+
+};
---
+++ test/integration/seed/test/seed1.js
@@ -0,0 +1,3 @@
+exports.seed = function(knex, promise) {
+  return 'seed 1';
+};
