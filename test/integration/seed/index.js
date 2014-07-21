4edc976880a9646fc8d010887203b0fd9dc1b812
Noah Santorello <nsantorello@gmail.com>
2014-07-21 19:33:42 -0400

    Finally seed tests green.

--- test/integration/seed/index.js
+++ test/integration/seed/index.js
@@ -17,7 +17,7 @@ module.exports = function(knex) {
 
   describe('knex.seed.run', function() {
     it('should run all seed files in the configured seed directory', function() {
-      return knex.seed.run({directory: 'test/integration/seed/test'}).then(function(data) {
+      return knex.seed.run({directory: 'test/integration/seed/test'}).spread(function(data) {
         expect(path.basename(data[0])).to.equal('seed1.js');
         expect(path.basename(data[1])).to.equal('seed2.js');
       });
