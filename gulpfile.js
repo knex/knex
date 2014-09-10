40e34e1e69176d756648ea455470ebd5a212f97f
Kris Reeves <kris.re@bbhmedia.com>
2014-09-10 12:15:58 -0400

    Don't lint leftover migration files from interrupted tests

--- gulpfile.js
+++ gulpfile.js
@@ -88,7 +88,10 @@ gulp.task('build', function() {
 
 // Run the test... TODO: split these out to individual components.
 gulp.task('jshint', function () {
-  gulp.src(['*.js', 'lib/**/*.js', 'test/**/*.js', '!test/coverage/**/*.js'])
+  gulp.src([
+      '*.js', 'lib/**/*.js', 'test/**/*.js',
+      '!test/coverage/**/*.js', '!test/integration/migrate/migrations/*.js'
+    ])
     .pipe(jshint())
     .pipe(jshint.reporter('default'))
     .pipe(jshint.reporter('fail'));
