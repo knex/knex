4b80797a4a03619784c7d2524593bfbd0cea5c07
Francis Gonzales <fgonzalestello91@gmail.com>
2015-12-16 13:29:13 -0500

    Dialect for firebird

---
+++ src/dialects/firebird/schema/compiler.js
@@ -0,0 +1,43 @@
+
+// Firebird Schema Compiler
+// -------
+var inherits       = require('inherits');
+var SchemaCompiler = require('../../../schema/compiler');
+var assign         = require('lodash/object/assign');
+
+function SchemaCompiler_Firebird(client, builder) {
+  SchemaCompiler.call(this, client, builder)
+}
+inherits(SchemaCompiler_Firebird, SchemaCompiler)
+
+assign(SchemaCompiler_Firebird.prototype, {
+
+  // Rename a table on the schema.
+  renameTable: function(tableName, to) {
+    this.pushQuery('rename table ' + this.formatter.wrap(tableName) + ' to ' + this.formatter.wrap(to));
+  },
+
+  // Check whether a table exists on the query.
+  hasTable: function(tableName) {
+    this.pushQuery({
+      sql: 'show tables like ' + this.formatter.parameter(tableName),
+      output: function(resp) {
+        return resp.length > 0;
+      }
+    });
+  },
+
+  // Check whether a column exists on the schema.
+  hasColumn: function(tableName, column) {
+    this.pushQuery({
+      sql: 'show columns from ' + this.formatter.wrap(tableName) +
+        ' like ' + this.formatter.parameter(column),
+      output: function(resp) {
+        return resp.length > 0;
+      }
+    });
+  }
+
+})
+
+module.exports = SchemaCompiler_Firebird;
