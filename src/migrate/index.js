knex/knex

commit 1d27fb85507c7c521e13bfcb296f38bc24fb57cb

Update lodash from 3.7.0 -> 4.6.0

src/migrate/index.js
@@ -4,11 +4,10 @@
 
 var fs       = require('fs');
 var path     = require('path');
-var _        = require('lodash');
 var mkdirp   = require('mkdirp');
 var Promise  = require('../promise');
 var helpers  = require('../helpers');
-var assign   = require('lodash/object/assign');
+import {assign, difference, chain, filter, includes, map, template, get, isBoolean, each, isEmpty, isUndefined, bind} from 'lodash'
 var inherits = require('inherits');
 
 function LockError(msg) {
@@ -33,7 +32,7 @@ export default class Migrator {
     return this._migrationData()
       .tap(validateMigrationList)
       .spread((all, completed) => {
-        return this._runBatch(_.difference(all, completed), 'up');
+        return this._runBatch(difference(all, completed), 'up');
       })
   }
 
@@ -45,7 +44,7 @@ export default class Migrator {
         .tap(validateMigrationList)
         .then((val) => this._getLastBatch(val))
         .then((migrations) => {
-          return this._runBatch(_.pluck(migrations, 'name'), 'down');
+          return this._runBatch(map(migrations, 'name'), 'down');
         });
     })
   }
@@ -70,10 +69,10 @@ export default class Migrator {
     this.config = this.setConfig(config);
     return this._listCompleted(config)
       .then((completed) => {
-        var val = _.chain(completed).map(function(value) {
+        var val = chain(completed).map(function(value) {
           return value.split('_')[0];
         }).max().value();
-        return (val === -Infinity ? 'none' : val);
+        return (isUndefined(val) ? 'none' : val);
       })
   }
 
@@ -98,9 +97,9 @@ export default class Migrator {
     this.config = this.setConfig(config);
     return Promise.promisify(fs.readdir, fs)(this._absoluteConfigDir())
       .then((migrations) => {
-        return _.filter(migrations, function(value) {
+        return filter(migrations, function(value) {
           var extension = path.extname(value);
-          return _.contains(['.co', '.coffee', '.eg', '.iced', '.js', '.litcoffee', '.ls'], extension);
+          return includes(['.co', '.coffee', '.eg', '.iced', '.js', '.litcoffee', '.ls'], extension);
         }).sort();
       })
   }
@@ -187,7 +186,7 @@ export default class Migrator {
   // Run a batch of current migrations, in sequence.
   _runBatch(migrations, direction) {
     return this._getLock()
-    .then(() => Promise.all(_.map(migrations, this._validateMigrationStructure, this)))
+    .then(() => Promise.all(map(migrations, bind(this._validateMigrationStructure, this))))
     .then(() => this._latestBatchNumber())
     .then(batchNo => {
       if (direction === 'up') batchNo++;
@@ -235,7 +234,7 @@ export default class Migrator {
     var tableName = this.config.tableName
     return this._ensureTable(tableName)
       .then(() => this.knex(tableName).orderBy('id').select('name'))
-      .then((migrations) => _.pluck(migrations, 'name'))
+      .then((migrations) => map(migrations, 'name'))
   }
 
   // Gets the migration list from the specified migration directory,
@@ -252,7 +251,7 @@ export default class Migrator {
   _generateStubTemplate() {
     var stubPath = this.config.stub || path.join(__dirname, 'stub', this.config.extension + '.stub');
     return Promise.promisify(fs.readFile, fs)(stubPath).then(function(stub) {
-      return _.template(stub.toString(), null, {variable: 'd'});
+      return template(stub.toString(), null, {variable: 'd'});
     });
   }
 
@@ -293,9 +292,9 @@ export default class Migrator {
   // transaction for a single migration by will, regardless of the common
   // config.
   _useTransaction(migration, allTransactionsDisabled) {
-    var singleTransactionValue = _.get(migration, 'config.transaction');
+    var singleTransactionValue = get(migration, 'config.transaction');
 
-    return _.isBoolean(singleTransactionValue) ?
+    return isBoolean(singleTransactionValue) ?
       singleTransactionValue :
       !allTransactionsDisabled;
   }
@@ -308,7 +307,7 @@ export default class Migrator {
     var directory = this._absoluteConfigDir()
     var current   = Promise.bind({failed: false, failedOn: 0});
     var log       = [];
-    _.each(migrations, (migration) => {
+    each(migrations, (migration) => {
       var name  = migration;
       migration = require(directory + '/' + name);
 
@@ -363,8 +362,8 @@ export default class Migrator {
 function validateMigrationList(migrations) {
   var all = migrations[0];
   var completed = migrations[1];
-  var diff = _.difference(completed, all);
-  if (!_.isEmpty(diff)) {
+  var diff = difference(completed, all);
+  if (!isEmpty(diff)) {
     throw new Error(
       'The migration directory is corrupt, the following files are missing: ' + diff.join(', ')
     );

src/dialects/maria/index.js
@@ -2,13 +2,13 @@
 // MariaSQL Client
 // -------
 var inherits      = require('inherits')
-var assign        = require('lodash/object/assign')
 var Client_MySQL  = require('../mysql')
 var Promise       = require('../../promise')
 var SqlString     = require('../../query/string')
 var helpers       = require('../../helpers')
-var pluck         = require('lodash/collection/pluck')
 var Transaction   = require('./transaction')
+
+import {assign, map} from 'lodash'
 
 function Client_MariaSQL(config) {
   Client_MySQL.call(this, config)
@@ -104,7 +104,7 @@ assign(Client_MariaSQL.prototype, {
       case 'pluck':
       case 'first':
         var resp = helpers.skim(rows);
-        if (method === 'pluck') return pluck(resp, obj.pluck);
+        if (method === 'pluck') return map(resp, obj.pluck);
         return method === 'first' ? resp[0] : resp;
       case 'insert':
         return [data.insertId];

src/dialects/mssql/index.js
@@ -1,9 +1,10 @@
 
 // MSSQL Client
 // -------
-var _              = require('lodash')
+import {map, flatten, values} from 'lodash'
 var inherits       = require('inherits')
-var assign         = require('lodash/object/assign')
+
+import {assign} from 'lodash'
 
 var Formatter      = require('./formatter')
 var Client         = require('../../client')
@@ -85,12 +86,12 @@ assign(Client_MSSQL.prototype, {
   },
 
   prepBindings: function(bindings) {
-    return _.map(bindings, function(value) {
+    return map(bindings, (value) => {
       if (value === undefined) {
         return this.valueForUndefined
       }
       return value
-    }, this)
+    })
   },
 
   // Grab a connection, run the query via the MSSQL streaming interface,
@@ -159,7 +160,7 @@ assign(Client_MSSQL.prototype, {
       case 'pluck':
       case 'first':
         response = helpers.skim(response)
-        if (method === 'pluck') return _.pluck(response, obj.pluck)
+        if (method === 'pluck') return map(response, obj.pluck)
         return method === 'first' ? response[0] : response
       case 'insert':
       case 'del':
@@ -173,7 +174,7 @@ assign(Client_MSSQL.prototype, {
             return response;
           }
           // return an array with values if only one returning value was specified
-          return _.flatten(_.map(response, _.values));
+          return flatten(map(response, values));
         }
         return response;
       default:

src/dialects/mysql/index.js
@@ -2,7 +2,6 @@
 // MySQL Client
 // -------
 var inherits       = require('inherits')
-var assign         = require('lodash/object/assign')
 
 var Client         = require('../../client')
 var Promise        = require('../../promise')
@@ -13,7 +12,8 @@ var QueryCompiler  = require('./query/compiler')
 var SchemaCompiler = require('./schema/compiler')
 var TableCompiler  = require('./schema/tablecompiler')
 var ColumnCompiler = require('./schema/columncompiler')
-var pluck          = require('lodash/collection/pluck')
+
+import {assign, map} from 'lodash'
 
 // Always initialize with the "QueryBuilder" and "QueryCompiler"
 // objects, which extend the base 'lib/query/builder' and
@@ -108,7 +108,7 @@ assign(Client_MySQL.prototype, {
       case 'pluck':
       case 'first':
         var resp = helpers.skim(rows)
-        if (method === 'pluck') return pluck(resp, obj.pluck)
+        if (method === 'pluck') return map(resp, obj.pluck)
         return method === 'first' ? resp[0] : resp
       case 'insert':
         return [rows.insertId]

src/dialects/mysql2/index.js
@@ -5,9 +5,7 @@ var inherits     = require('inherits')
 var Client_MySQL = require('../mysql')
 var Promise      = require('../../promise')
 var helpers      = require('../../helpers')
-var pick         = require('lodash/object/pick')
-var pluck        = require('lodash/collection/pluck')
-var assign       = require('lodash/object/assign');
+import {pick, map, assign} from 'lodash'
 var Transaction  = require('./transaction')
 
 var configOptions = [
@@ -84,7 +82,7 @@ assign(Client_MySQL2.prototype, {
       case 'pluck':
       case 'first':
         var resp = helpers.skim(rows)
-        if (method === 'pluck') return pluck(resp, obj.pluck)
+        if (method === 'pluck') return map(resp, obj.pluck)
         return method === 'first' ? resp[0] : resp
       case 'insert':
         return [rows.insertId]

src/dialects/oracle/index.js
@@ -1,15 +1,14 @@
 
 // Oracle Client
 // -------
-var _                 = require('lodash')
-var inherits          = require('inherits')
-var assign            = require('lodash/object/assign')
+import {assign, map, flatten, values} from 'lodash'
 
-var Formatter         = require('./formatter')
-var Client            = require('../../client')
-var Promise           = require('../../promise')
-var helpers           = require('../../helpers')
-var SqlString         = require('../../query/string')
+var inherits  = require('inherits')
+var Formatter = require('./formatter')
+var Client    = require('../../client')
+var Promise   = require('../../promise')
+var helpers   = require('../../helpers')
+var SqlString = require('../../query/string')
 
 var Transaction       = require('./transaction')
 var QueryCompiler     = require('./query/compiler')
@@ -53,7 +52,7 @@ assign(Client_Oracle.prototype, {
   TableCompiler: TableCompiler,
 
   prepBindings: function(bindings) {
-    return _.map(bindings, function(value) {
+    return map(bindings, (value) => {
       // returning helper uses always ROWID as string
       if (value instanceof ReturningHelper && this.driver) {
         return new this.driver.OutParam(this.driver.OCCISTRING)
@@ -68,7 +67,7 @@ assign(Client_Oracle.prototype, {
         return this.valueForUndefined
       }
       return value
-    }, this)
+    })
   },
 
   // Get a raw connection, called by the `pool` whenever a new
@@ -153,7 +152,7 @@ assign(Client_Oracle.prototype, {
       case 'pluck':
       case 'first':
         response = helpers.skim(response);
-        if (obj.method === 'pluck') response = _.pluck(response, obj.pluck);
+        if (obj.method === 'pluck') response = map(response, obj.pluck);
         return obj.method === 'first' ? response[0] : response;
       case 'insert':
       case 'del':
@@ -164,7 +163,7 @@ assign(Client_Oracle.prototype, {
             return response;
           }
           // return an array with values if only one returning value was specified
-          return _.flatten(_.map(response, _.values));
+          return flatten(map(response, values));
         }
         return response.updateCount;
       default:

src/dialects/postgres/index.js
@@ -1,12 +1,11 @@
 
 // PostgreSQL
 // -------
-var _              = require('lodash')
+import {assign, map, extend} from 'lodash'
 var inherits       = require('inherits')
 var Client         = require('../../client')
 var Promise        = require('../../promise')
 var utils          = require('./utils')
-var assign         = require('lodash/object/assign')
 
 var QueryCompiler  = require('./query/compiler')
 var ColumnCompiler = require('./schema/columncompiler')
@@ -53,9 +52,9 @@ assign(Client_PG.prototype, {
 
   // Prep the bindings as needed by PostgreSQL.
   prepBindings: function(bindings, tz) {
-    return _.map(bindings, function(binding) {
+    return map(bindings, (binding) => {
       return utils.prepareValue(binding, tz, this.valueForUndefined)
-    }, this);
+    });
   },
 
   // Get a raw connection, called by the `pool` whenever a new
@@ -144,7 +143,7 @@ assign(Client_PG.prototype, {
   // and any other necessary prep work.
   _query: function(connection, obj) {
     var sql = obj.sql = this.positionBindings(obj.sql)
-    if (obj.options) sql = _.extend({text: sql}, obj.options);
+    if (obj.options) sql = extend({text: sql}, obj.options);
     return new Promise(function(resolver, rejecter) {
       connection.query(sql, obj.bindings, function(err, response) {
         if (err) return rejecter(err);
@@ -162,7 +161,7 @@ assign(Client_PG.prototype, {
     var returning = obj.returning;
     if (resp.command === 'SELECT') {
       if (obj.method === 'first') return resp.rows[0];
-      if (obj.method === 'pluck') return _.pluck(resp.rows, obj.pluck);
+      if (obj.method === 'pluck') return map(resp.rows, obj.pluck);
       return resp.rows;
     }
     if (returning) {

src/dialects/sqlite3/index.js
@@ -1,12 +1,10 @@
 
 // SQLite3
 // -------
-var _              = require('lodash')
 var Promise        = require('../../promise')
 
 var inherits       = require('inherits')
-var assign         = require('lodash/object/assign')
-var pluck          = require('lodash/collection/pluck');
+import {isUndefined, map, assign} from 'lodash'
 
 var Client         = require('../../client')
 var helpers        = require('../../helpers')
@@ -19,7 +17,7 @@ var SQLite3_DDL    = require('./schema/ddl')
 
 function Client_SQLite3(config) {
   Client.call(this, config)
-  if (_.isUndefined(config.useNullAsDefault)) {
+  if (isUndefined(config.useNullAsDefault)) {
     helpers.warn('sqlite does not support inserting default values. Set the `useNullAsDefault` flag to hide this warning. (see docs http://knexjs.org/#Builder-insert).');
   }
 }
@@ -113,13 +111,13 @@ assign(Client_SQLite3.prototype, {
   },
 
   prepBindings: function(bindings) {
-    return _.map(bindings, function(binding) {
+    return map(bindings, (binding) => {
       if (binding === undefined && this.valueForUndefined !== null) {
         throw new TypeError("`sqlite` does not support inserting default values. Specify values explicitly or use the `useNullAsDefault` config flag. (see docs http://knexjs.org/#Builder-insert).");
       } else {
         return binding
       }
-    }, this);
+    });
   },
 
   // Ensures the response is returned in the same format as other clients.
@@ -132,7 +130,7 @@ assign(Client_SQLite3.prototype, {
       case 'pluck':
       case 'first':
         response = helpers.skim(response)
-        if (obj.method === 'pluck') response = pluck(response, obj.pluck)
+        if (obj.method === 'pluck') response = map(response, obj.pluck)
         return obj.method === 'first' ? response[0] : response;
       case 'insert':
         return [ctx.lastID];

commit 6bcceb4f49bbab26bd1828b0c30120fd775fab46

Prevent crash on `migrate:make`
Fixes error introduced in 1d27fb8. `template` fails if passed a `null` second argument.

Fixes #1385

src/migrate/index.js
@@ -258,7 +258,7 @@ export default class Migrator {
   _generateStubTemplate() {
     var stubPath = this.config.stub || path.join(__dirname, 'stub', this.config.extension + '.stub');
     return Promise.promisify(fs.readFile, {context: fs})(stubPath).then(function(stub) {
-      return template(stub.toString(), null, {variable: 'd'});
+      return template(stub.toString(), {variable: 'd'});
     });
   }
 