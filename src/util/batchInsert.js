b882bdf4f110e2a69689617e896f052df34c69b5
wubzz <wubzz94@hotmail.com>
2016-05-09 19:03:48 +0200

    Fix indentation

--- src/util/batchInsert.js
+++ src/util/batchInsert.js
@@ -4,8 +4,8 @@ import {isNumber, isString, isArray, chunk, flatten} from 'lodash';
 import Promise from '../promise';
 
 export default class BatchInsert {
-	constructor (client, tableName, batch, chunkSize = 1000) {
-		if (!isNumber(chunkSize) || chunkSize < 1) {
+	constructor(client, tableName, batch, chunkSize = 1000) {
+		if(!isNumber(chunkSize) || chunkSize < 1) {
 			throw new TypeError(`Invalid chunkSize: ${chunkSize}`);
 		}
 
@@ -25,7 +25,7 @@ export default class BatchInsert {
 	 * Columns to return from the batch operation.
 	 * @param returning
 	 */
-	returning (returning) {
+	returning(returning) {
 		if(isArray(returning) || isString(returning)) {
 			this._returning = returning;
 		}
@@ -37,16 +37,16 @@ export default class BatchInsert {
 	 * If this is the case, don't autoCommit their transaction. The responsibility falls on the user.
 	 * @param transaction
 	 */
-	transacting (transaction) {
+	transacting(transaction) {
 		this._transaction = transaction;
-		this._autoCommit = false;
+		this._autoCommit  = false;
 		return this;
 	}
 
-	then (callback = function() {}) {
+	then(callback = function() {}) {
 		let transaction;
 		return Promise.resolve()
-		.then(() => {
+			.then(() => {
 				if(this._transaction) {
 					transaction = this._transaction;
 					return Promise.resolve();
@@ -58,7 +58,7 @@ export default class BatchInsert {
 					});
 				});
 			})
-		.then(() => {
+			.then(() => {
 				return Promise.all(this.batch.map((items) => {
 					let promise = transaction(this.tableName)
 						.insert(items);
@@ -67,13 +67,13 @@ export default class BatchInsert {
 					}
 					return promise;
 				}));
-		})
-		.then((result) => {
+			})
+			.then((result) => {
 				if(this._autoCommit) {
 					transaction.commit();
 				}
 				return callback(flatten(result || []));
-		})
+			})
 			.catch((error) => {
 				if(this._autoCommit) {
 					transaction.rollback(error);
