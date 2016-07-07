fcb0b8e8e9b6d739097f677bae7996ce4886b114
Vladimir Dronnikov <dronnikov@gmail.com>
2016-07-07 07:38:31 +0300

    formatting

--- src/dialects/memsql/schema/columnbuilder.js
+++ src/dialects/memsql/schema/columnbuilder.js
@@ -1,16 +1,16 @@
 
-import inherits from 'inherits';
-import ColumnBuilder from '../../../schema/columnbuilder';
+import inherits from 'inherits'
+import ColumnBuilder from '../../../schema/columnbuilder'
 
 function ColumnBuilder_MemSQL() {
-  ColumnBuilder.apply(this, arguments);
+  ColumnBuilder.apply(this, arguments)
 }
-inherits(ColumnBuilder_MemSQL, ColumnBuilder);
+inherits(ColumnBuilder_MemSQL, ColumnBuilder)
 
 // computed properties
 ColumnBuilder_MemSQL.prototype.computed = function (definition) {
-  this._modifiers.computed = definition;
-  return this;
-};
+  this._modifiers.computed = definition
+  return this
+}
 
 export default ColumnBuilder_MemSQL
