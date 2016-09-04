10ea3e6ae153b93ab4617ad46b00083963d77e75
Danny Fritz <dannyfritz@gmail.com>
2016-09-04 15:34:57 -0600

    🚨 Node 0.10 won't run test with ES6

--- test/unit/knexfile-test.js
+++ test/unit/knexfile-test.js
@@ -1,34 +1,15 @@
-/*global expect, describe, it, beforeEach, afterEach*/
+/*global expect, describe, it*/
 
 'use strict';
 
-// const sinon = require('sinon')
-
 describe('Bad knexfile.js', function() {
-  // let sandbox
-
-  // beforeEach(function() {
-  //   sandbox = sinon.sandbox.create()
-  // });
-
-  // afterEach(function() {
-  //   sandbox.restore()
-  // })
 
   it('should throw an error when a bad client is supplied', function() {
-    // sandbox.stub(console, 'log')
-    const knex = require('../../knex');
+    var knex = require('../../knex');
     expect(function () {
       knex({client: 'badclient'})
     })
     .to.throw(/Cannot find module '\.\/dialects\/badclient\/index.js'/)
-    // expect(console.log.calledOnce).to.be.true;
-    // expect(console.log.calledWith(
-    //   `Client or dialect provided, "badclient", ` +
-    //   `to the knex configuration was not found. ` +
-    //   `Did you make a typo? ` +
-    //   `Example dialects: "maria", "pg", etc.`)
-    // ).to.be.true;
   });
 
 });
