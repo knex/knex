895eaebe012a7c4e598c3757de20df5e4b51d294
Tim Griesser <tgriesser10@gmail.com>
2016-10-09 18:35:58 -0400

    Update pool docs

--- sections/installation.js
+++ sections/installation.js
@@ -160,8 +160,7 @@ export default [
   {
     type: "text",
     content: [
-      "The client created by the configuration initializes a connection pool, using the [generic-pool](https://github.com/myndzi/pool2) library. This connection pool has a default setting of a `min: 2, max: 10` for the MySQL and PG libraries, and a single connection for sqlite3 (due to issues with utilizing multiple connections on a single file). To change the config settings for the pool, pass a `pool` option as one of the keys in the initialize block.",
-      "The default connection pool also applies a `ping` function to verify that a connection is still alive by running a `SELECT 1` query. If you don't want this check you may supply your own ping function to overwrite knex's default behavior.",
+      "The client created by the configuration initializes a connection pool, using the [generic-pool](https://github.com/coopernurse/node-pool) library. This connection pool has a default setting of a `min: 2, max: 10` for the MySQL and PG libraries, and a single connection for sqlite3 (due to issues with utilizing multiple connections on a single file). To change the config settings for the pool, pass a `pool` option as one of the keys in the initialize block.",
       "Checkout the [generic-pool](https://github.com/coopernurse/node-pool) library for more information."
     ]
   },
