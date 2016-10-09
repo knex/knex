5a1816693b8ed9571031dba02d05aa26ffbc3f40
Tim Griesser <tgriesser10@gmail.com>
2016-10-09 14:49:57 -0400

    Fix tests

--- test/unit/query/builder.js
+++ test/unit/query/builder.js
@@ -3959,7 +3959,7 @@ describe("QueryBuilder", function() {
   });
 
   it('#1710, properly escapes arrays in where clauses in postgresql', () => {
-    testquery(qb().select('*').from('sometable').where('array_field', '&&', [7]), {
+    testquery(qb => qb.select('*').from('sometable').where('array_field', '&&', [7]), {
       postgres: `select * from "sometable" where "array_field" && '{7}'`
     });
   })
