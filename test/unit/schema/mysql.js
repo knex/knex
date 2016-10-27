3c7602d11442b4fc743ab241a26d9b574f81183f
RWOverdijk <r.w.overdijk@gmail.com>
2016-10-27 15:29:20 +0200

    Added tests for .alter()

--- test/unit/schema/mysql.js
+++ test/unit/schema/mysql.js
@@ -461,6 +461,17 @@ describe(dialect + " SchemaBuilder", function() {
     expect(tableSql[0].sql).to.equal('alter table `users` add `foo` decimal(2, 6)');
   });
 
+  it('should alter columns with the alter flag', function() {
+    tableSql = client.schemaBuilder().table('users', function() {
+      this.string('foo').alter();
+      this.string('bar');
+    }).toSQL();
+
+    equal(2, tableSql.length);
+    expect(tableSql[0].sql).to.equal('alter table `users` add `bar` varchar(255)');
+    expect(tableSql[1].sql).to.equal('alter table `users` modify `foo` varchar(255)');
+  });
+
   it('is possible to set raw statements in defaultTo, #146', function() {
     tableSql = client.schemaBuilder().createTable('default_raw_test', function(t) {
       t.timestamp('created_at').defaultTo(client.raw('CURRENT_TIMESTAMP'));
