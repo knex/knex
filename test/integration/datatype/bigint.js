20b524e7efb1d7f8c7e1ed44d3fc66c3478a7221
Nikita Khomyakov <dr.evil@krtn.eu>
2016-11-24 10:22:21 +0000

    Add test case for decimal number handling in MSSQL

--- test/integration/datatype/bigint.js
+++ test/integration/datatype/bigint.js
@@ -70,4 +70,29 @@ module.exports = function (knex) {
             expect(err).to.be.undefined;
         });
     });
+
+    it('#1781 - decimal value must not be converted to integer', function () {
+        if (!/mssql/i.test(knex.client.dialect)) {
+            return
+        }
+
+        var tableName = 'decimal_test'
+        var value = 123.4567
+
+        return knex.transaction(function (tr) {
+            return tr.schema.dropTableIfExists(tableName)
+                .then(function () {
+                    return tr.schema.createTable(tableName, function (table) {
+                        table.increments()
+                        table.decimal('value', 14, 4).notNullable()
+                    })
+                })
+        }).then(function () {
+            return knex(tableName).insert({ value: value })
+        }).then(function () {
+            return knex(tableName).first('value')
+        }).then(function (response) {
+            expect(response.value).to.be.eql(value)
+        })
+    })
 };
