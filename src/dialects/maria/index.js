1c0fbc0a9465eab9c3517ad85ca2be8c563d55cb
wubzz <wubzz94@hotmail.com>
2017-06-16 15:48:33 +0200

    Upgrade generic-pool to v3.1.7 (Attempt)

--- src/dialects/maria/index.js
+++ src/dialects/maria/index.js
@@ -44,7 +44,10 @@ assign(Client_MariaSQL.prototype, {
   },
 
   validateConnection(connection) {
-    return connection.connected === true
+    if(connection.connected === true) {
+      return Promise.resolve();
+    }
+    return Promise.reject();
   },
 
   // Used to explicitly close a connection, called internally by the pool
--- src/dialects/mysql/index.js
+++ src/dialects/mysql/index.js
@@ -85,7 +85,10 @@ assign(Client_MySQL.prototype, {
   },
 
   validateConnection(connection) {
-    return connection.state === 'connected' || connection.state === 'authenticated'
+    if(connection.state === 'connected' || connection.state === 'authenticated') {
+      return Promise.resolve();
+    }
+    return Promise.reject();
   },
 
   // Grab a connection, run the query via the MySQL streaming interface,
