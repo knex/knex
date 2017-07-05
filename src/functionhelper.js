9e644921d729e5e68e7282db728b24ec56ecf908
Kieron Wiltshire <KieronWiltshire@users.noreply.github.com>
2017-07-05 14:35:29 +0100

    Added UUID() helper function

--- src/functionhelper.js
+++ src/functionhelper.js
@@ -9,4 +9,8 @@ FunctionHelper.prototype.now = function() {
   return this.client.raw('CURRENT_TIMESTAMP')
 }
 
+FunctionHelper.prototype.uuid = function() {
+  return this.client.raw('UUID()') 
+}
+
 export default FunctionHelper
