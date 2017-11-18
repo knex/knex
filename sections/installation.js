b8b904f63496b5a17d2002d697a77f25324fda94
wubzz <wubzz94@hotmail.com>
2017-11-18 16:28:49 +0100

    Change example of searchPath to Array syntax

--- sections/installation.js
+++ sections/installation.js
@@ -85,7 +85,7 @@ export default [
       var pg = require('knex')({
         client: 'pg',
         connection: process.env.PG_CONNECTION_STRING,
-        searchPath: 'knex,public'
+        searchPath: ['knex', 'public'],
       });
     `
   },
