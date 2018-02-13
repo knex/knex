0d2869c985a55b606116071725f958feb94448aa
root <root@debian>
2018-02-13 15:29:02 +0100

    Removed section Upgrading and rebuilt 0.14.3 docs

--- sections/upgrading.js
+++ sections/upgrading.js
@@ -1,14 +1,1 @@
-export default [
-	{
-		type: "heading",
-		size: "lg",
-		content: "Upgrading 0.13 -> 0.14",
-	},
-	{
-		type: "text",
-		content: [
-			"[generic-pool](https://github.com/coopernurse/node-pool) was upgraded to v3. If you have specified `idleTimeoutMillis` or `softIdleTimeoutMillis` in the pool config then you will need to add `evictionRunIntervalMillis: 1000` when upgrading to 0.14.",
-			"See original [issue #2322](https://github.com/tgriesser/knex/issues/2322) for details."
-		]
-	},
-]
+export default []
