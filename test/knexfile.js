10ad90d368b3fb932c29fc33cb890da026724895
Igor Savin <isavin@shift4.com>
2018-04-04 15:30:27 +0300

    Restore more removed code

--- test/knexfile.js
+++ test/knexfile.js
@@ -44,17 +44,115 @@ var seeds = {
 
 var testConfigs = {
 
+  maria: {
+    dialect: 'maria',
+    connection: testConfig.maria || {
+      db: "knex_test",
+      user: "root",
+      charset: 'utf8',
+      host: '127.0.0.1'
+    },
+    pool: mariaPool,
+    migrations: migrations,
+    seeds: seeds
+  },
+
+  mysql: {
+    dialect: 'mysql',
+    connection: testConfig.mysql || {
+      database: "knex_test",
+      user: "root",
+      charset: 'utf8'
+    },
+    pool: mysqlPool,
+    migrations: migrations,
+    seeds: seeds,
+  },
+
+  mysql2: {
+    dialect: 'mysql2',
+    connection: testConfig.mysql || {
+      database: "knex_test",
+      user: "root",
+      charset: 'utf8'
+    },
+    pool: mysqlPool,
+    migrations: migrations,
+    seeds: seeds,
+  },
+
+  oracle: {
+    client: 'strong-oracle',
+    connection: testConfig.oracle || {
+      adapter:  "oracle",
+      database: "knex_test",
+      user:     "oracle"
+    },
+    pool: pool,
+    migrations: migrations
+  },
+
+  oracledb: {
+    client: 'oracledb',
+    connection: testConfig.oracledb || {
+      user          : "travis",
+      password      : "travis",
+      connectString : "localhost/XE",
+      // https://github.com/oracle/node-oracledb/issues/525
+      stmtCacheSize : 0
+    },
+    pool: pool,
+    migrations: migrations
+  },
+
   postgres: {
     dialect: 'postgres',
     connection: testConfig.postgres || {
       adapter:  "postgresql",
       database: "knex_test",
-      user:     "postgres",
-      password: "pass"
+      user:     "postgres"
     },
     pool: pool,
     migrations: migrations,
     seeds: seeds,
+  },
+
+  redshift: {
+    dialect: 'redshift',
+    connection: testConfig.redshift || {
+      adapter:  'postgresql',
+      database: 'knex_test',
+      user:     process.env.REDSHIFT_USER || 'postgres',
+      password: process.env.REDSHIFT_PASSWORD || '',
+      port:     '5439',
+      host:     process.env.REDSHIFT_HOST || '127.0.0.1',
+    },
+    pool: pool,
+    migrations: migrations,
+    seeds: seeds
+  },
+
+  sqlite3: {
+    dialect: 'sqlite3',
+    connection: testConfig.sqlite3 || {
+      filename: __dirname + '/test.sqlite3'
+    },
+    pool,
+    migrations: migrations,
+    seeds: seeds
+  },
+
+  mssql: {
+    dialect: 'mssql',
+    connection: testConfig.mssql || {
+      user: "sa",
+      password: "S0meVeryHardPassword",
+      server: "localhost",
+      database: "knex_test"
+    },
+    pool: pool,
+    migrations: migrations,
+    seeds: seeds
   }
 };
 
