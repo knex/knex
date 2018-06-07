c3b18e1dd5e26e1ffd071090371174a59f7c26c9
Tyler Watson <tyler@tw.id.au>
2018-06-07 19:24:35 +1000

    wip: translating node-mssql to tedious

--- src/dialects/mssql/index.js
+++ src/dialects/mssql/index.js
@@ -1,30 +1,33 @@
-
 // MSSQL Client
 // -------
-import { assign, map, flatten, values } from 'lodash'
-import inherits from 'inherits';
+import { assign, map, flatten, values, mapValues } from "lodash";
+import inherits from "inherits";
 
-import Client from '../../client';
-import Promise from 'bluebird';
+import Client from "../../client";
+import Promise from "bluebird";
 
-import Formatter from '../../formatter'
-import Transaction from './transaction';
-import QueryCompiler from './query/compiler';
-import SchemaCompiler from './schema/compiler';
-import TableCompiler from './schema/tablecompiler';
-import ColumnCompiler from './schema/columncompiler';
+import Formatter from "../../formatter";
+import Transaction from "./transaction";
+import QueryCompiler from "./query/compiler";
+import SchemaCompiler from "./schema/compiler";
+import TableCompiler from "./schema/tablecompiler";
+import ColumnCompiler from "./schema/columncompiler";
+
+import * as tedious from "tedious";
+
+const debug = require("debug")("knex:mssql");
 
 const { isArray } = Array;
 
-const SQL_INT4 = { MIN : -2147483648, MAX: 2147483647}
-const SQL_BIGINT_SAFE = { MIN : -9007199254740991, MAX: 9007199254740991}
+const SQL_INT4 = { MIN: -2147483648, MAX: 2147483647 };
+const SQL_BIGINT_SAFE = { MIN: -9007199254740991, MAX: 9007199254740991 };
 
 // Always initialize with the "QueryBuilder" and "QueryCompiler" objects, which
 // extend the base 'lib/query/builder' and 'lib/query/compiler', respectively.
 function Client_MSSQL(config) {
   // #1235 mssql module wants 'server', not 'host'. This is to enforce the same
   // options object across all dialects.
-  if(config && config.connection && config.connection.host) {
+  if (config && config.connection && config.connection.host) {
     config.connection.server = config.connection.host;
   }
   Client.call(this, config);
@@ -32,176 +35,312 @@ function Client_MSSQL(config) {
 inherits(Client_MSSQL, Client);
 
 assign(Client_MSSQL.prototype, {
+  dialect: "mssql",
 
-  dialect: 'mssql',
-
-  driverName: 'mssql',
+  driverName: "mssql",
 
   _driver() {
-    return require('mssql');
+    return tedious;
   },
 
   formatter() {
-    return new MSSQL_Formatter(this, ...arguments)
+    return new MSSQL_Formatter(this, ...arguments);
   },
 
   transaction() {
-    return new Transaction(this, ...arguments)
+    return new Transaction(this, ...arguments);
   },
 
   queryCompiler() {
-    return new QueryCompiler(this, ...arguments)
+    return new QueryCompiler(this, ...arguments);
   },
 
   schemaCompiler() {
-    return new SchemaCompiler(this, ...arguments)
+    return new SchemaCompiler(this, ...arguments);
   },
 
   tableCompiler() {
-    return new TableCompiler(this, ...arguments)
+    return new TableCompiler(this, ...arguments);
   },
 
   columnCompiler() {
-    return new ColumnCompiler(this, ...arguments)
+    return new ColumnCompiler(this, ...arguments);
   },
 
   wrapIdentifierImpl(value) {
-    return (value !== '*' ? `[${value.replace(/\[/g, '[')}]` : '*')
+    return value !== "*" ? `[${value.replace(/\[/g, "[")}]` : "*";
   },
 
   // Get a raw connection, called by the `pool` whenever a new
   // connection needs to be added to the pool.
   acquireRawConnection() {
-    return new Promise((resolver, rejecter) => {
-      const connection = new this.driver.ConnectionPool(this.connectionSettings);
-      connection.connect((err) => {
-        if (err) {
-          return rejecter(err)
+    return new Promise((resolve, reject) => {
+      const config = Object.assign({}, { 
+        server: this.config.connection.host,
+        userName: this.config.connection.user,
+        options: {
+          useColumnNames: true,
         }
-        connection.on('error', (err) => {
-          connection.__knex__disposed = err
-        })
-        resolver(connection);
+      }, this.config.connection);
+
+      const connection = new tedious.Connection(config);
+      debug("connection::connection new connection requested");
+
+      connection.once("connect", err => {
+        if (err) {
+          debug("connection::connect error: %s", err.message);
+          return reject(err);
+        }
+
+        debug("connection::connect connected to server");
+
+        connection.connected = true;
+        connection.on("error", e => {
+          debug("connection::error message=%s", err.message);
+          connection.__knex__disposed = e;
+        });
+
+        connection.once("end", () => {
+          connection.connected = false;
+          connection.__knex__disposed = "Connection to server was terminated.";
+          debug("connection::end connection ended.");
+        });
+
+        return resolve(connection);
       });
     });
   },
 
   validateConnection(connection) {
-    if (connection.connected === true) {
-      return true
-    }
-
-    return false
+    return connection && connection.connected;
   },
 
   // Used to explicitly close a connection, called internally by the pool
   // when a connection times out or the pool is shutdown.
   destroyRawConnection(connection) {
-    return connection.close()
+    debug("connection::destroy");
+    return new Promise((resolve, reject) => {
+      connection.once('end', () => {
+        resolve();
+      });
+
+      connection.close();
+    });
   },
 
   // Position the bindings for the query.
   positionBindings(sql) {
-    let questionCount = -1
+    let questionCount = -1;
     return sql.replace(/\?/g, function() {
-      questionCount += 1
-      return `@p${questionCount}`
-    })
+      questionCount += 1;
+      return `@p${questionCount}`;
+    });
   },
 
   // Grab a connection, run the query via the MSSQL streaming interface,
   // and pass that through to the stream we've sent back to the client.
   _stream(connection, obj, stream, options) {
-    options = options || {}
-    if (!obj || typeof obj === 'string') obj = {sql: obj}
+    options = options || {};
+    if (!obj || typeof obj === "string") obj = { sql: obj };
     return new Promise((resolver, rejecter) => {
-      stream.on('error', (err) => {
-        rejecter(err)
+      stream.on("error", err => {
+        rejecter(err);
       });
-      stream.on('end', resolver);
-      const { sql } = obj
-      if (!sql) return resolver()
+      stream.on("end", resolver);
+      const { sql } = obj;
+      if (!sql) return resolver();
       const req = (connection.tx_ || connection).request();
       //req.verbose = true;
       req.multiple = true;
       req.stream = true;
       if (obj.bindings) {
         for (let i = 0; i < obj.bindings.length; i++) {
-          this._setReqInput(req, i, obj.bindings[i])
+          this._setReqInput(req, i, obj.bindings[i]);
         }
       }
-      req.pipe(stream)
-      req.query(sql)
-    })
+      req.pipe(stream);
+      req.query(sql);
+    });
   },
 
   // Runs the query on the specified connection, providing the bindings
   // and any other necessary prep work.
-  _query(connection, obj) {
+  _queryOld(connection, obj) {
     const client = this;
-    if (!obj || typeof obj === 'string') obj = {sql: obj}
+    if (!obj || typeof obj === "string") obj = { sql: obj };
     return new Promise((resolver, rejecter) => {
-      const { sql } = obj
-      if (!sql) return resolver()
+      const { sql } = obj;
+      if (!sql) return resolver();
       const req = (connection.tx_ || connection).request();
       // req.verbose = true;
       req.multiple = true;
       if (obj.bindings) {
         for (let i = 0; i < obj.bindings.length; i++) {
-          client._setReqInput(req, i, obj.bindings[i])
+          const binding = obj.bindings[i];
+          const type = client._typeForBinding(binding);
+
+          client._setReqInput(req, i, binding, type);
         }
       }
       req.query(sql, (err, recordset) => {
         if (err) {
-          return rejecter(err)
+          return rejecter(err);
         }
-        obj.response = recordset.recordsets[0]
-        resolver(obj)
-      })
-    })
+        obj.response = recordset.recordsets[0];
+        resolver(obj);
+      });
+    });
+  },
+
+  _query(connection, query) {
+    const sql = typeof query === "string" ? query : query.sql;
+
+    return new Promise((resolve, reject) => {
+      const request = new tedious.Request(sql, (err, rowCount) => {
+        if (err) {
+          debug("request::error message=%s", err.message);
+          return reject(err);
+        }
+
+        if (!query.response) {
+          query.response = new Array(rowCount);
+        }
+        debug("request::callback rowCount=%d", rowCount);
+        resolve(query);
+      });
+
+      request.on('prepared', () => {
+        debug("request %s::request prepared", this.id);
+      });
+
+      request.on("row", row => {
+        debug("request::row");
+        if (!query.response) {
+          query.response = [];
+        }
+
+        query.response.push(mapValues(row, 'value'));
+      });
+
+      request.on("done", (rowCount, more) => {
+        debug("request::done rowCount=%d more=%s", rowCount, more);
+      });
+      
+      request.on("doneProc", (rowCount, more) => {
+        debug("request::doneProc id=%s rowCount=%d more=%s", request.id, rowCount, more);
+      });
+
+      request.on("doneInProc", (rowCount, more) => {
+        debug("request::doneInProc id=%s rowCount=%d more=%s", request.id, rowCount, more);
+      });
+
+      request.on('requestCompleted', () => {
+        debug("request::completed id=%s", request.id);
+      });
+
+      if (Array.isArray(query.bindings)) {
+        for (let i = 0; i < query.bindings.length; i++) {
+          const binding = query.bindings[i];
+          const type = this._typeForBinding(binding);
+
+          this._setReqInput(request, i, binding, type);
+          debug("request::binding pos=%d type=%s value=%s", i, type.name, binding);
+        }
+      }
+
+      connection.execSql(request);
+      debug("connection::query executing query");
+    });
   },
 
   // sets a request input parameter. Detects bigints and decimals and sets type appropriately.
-  _setReqInput(req, i, binding) {
-    if (typeof binding == 'number') {
+  _setReqInput(req, i, binding, type = tedious.TYPES.NVarChar) {
+    const tediousType = this._typeForBinding(binding);
+    const bindingName = "p".concat(i);
+    let scale;
+
+    if (typeof binding == "number") {
       if (binding % 1 !== 0) {
-        req.input(`p${i}`, this.driver.Decimal(38, 10), binding)
-      } else if (binding < SQL_INT4.MIN || binding > SQL_INT4.MAX) {
-        if (binding < SQL_BIGINT_SAFE.MIN || binding > SQL_BIGINT_SAFE.MAX) {
-          throw new Error(`Bigint must be safe integer or must be passed as string, saw ${binding}`)
-        }
-        req.input(`p${i}`, this.driver.BigInt, binding)
-      } else {
-        req.input(`p${i}`, this.driver.Int, binding)
+        scale = this._scaleForBinding(binding);
       }
-    } else {
-      req.input(`p${i}`, binding)
+    }
+
+    req.addParameter(bindingName, tediousType, binding, scale);
+  },
+
+  _scaleForBinding(binding) {
+    if (binding % 1 === 0) {
+      throw new Error(`The binding value ${binding} must be a decimal number.`);
+    }
+
+    return { scale: 10 };
+  },
+
+  _typeForBinding(binding) {
+    switch (typeof binding) {
+      case "string":
+        return tedious.TYPES.NVarChar;
+      case "boolean":
+        return tedious.TYPES.Bit;
+      case "number": {
+        if (binding % 1 !== 0) {
+          return tedious.TYPES.Decimal;
+        }
+
+        if (binding < SQL_INT4.MIN || binding > SQL_INT4.MAX) {
+          if (binding < SQL_BIGINT_SAFE.MIN || binding > SQL_BIGINT_SAFE.MAX) {
+            throw new Error(
+              `Bigint must be safe integer or must be passed as string, saw ${binding}`
+            );
+          }
+
+          return tedious.TYPES.BigInt;
+        }
+
+        return tedious.TYPES.Int;
+      }
+      default: {
+        if (binding === null) {
+          return tedious.TYPES.Null;
+        }
+
+        if (binding instanceof Date) {
+          return tedious.TYPES.DateTime;
+        }
+
+        return tedious.TYPES.NVarChar;
+      }
     }
   },
 
   // Process the response as returned from the query.
   processResponse(obj, runner) {
     if (obj == null) return;
-    const { response, method } = obj
-    if (obj.output) return obj.output.call(runner, response)
+    const { response, method } = obj;
+    if (obj.output) return obj.output.call(runner, response);
+
+    if (!response || !response.length) {
+      return;
+    }
+
     switch (method) {
-      case 'select':
-      case 'pluck':
-      case 'first':
-        if (method === 'pluck') return map(response, obj.pluck)
-        return method === 'first' ? response[0] : response
-      case 'insert':
-      case 'del':
-      case 'update':
-      case 'counter':
+      case "select":
+      case "pluck":
+      case "first":
+        if (method === "pluck") return map(response, obj.pluck);
+        return method === "first" ? mapValues(response[0], 'value') : response;
+      case "insert":
+      case "del":
+      case "update":
+      case "counter":
         if (obj.returning) {
-          if (obj.returning === '@@rowcount') {
-            return response[0]['']
+          if (obj.returning === "@@rowcount") {
+            return response[0][""];
           }
 
           if (
             (isArray(obj.returning) && obj.returning.length > 1) ||
-            obj.returning[0] === '*'
+            obj.returning[0] === "*"
           ) {
             return response;
           }
@@ -210,25 +349,23 @@ assign(Client_MSSQL.prototype, {
         }
         return response;
       default:
-        return response
+        return response;
     }
   }
-
-})
+});
 
 class MSSQL_Formatter extends Formatter {
-
   // Accepts a string or array of columns to wrap as appropriate.
   columnizeWithPrefix(prefix, target) {
-    const columns = typeof target === 'string' ? [target] : target
-    let str = '', i = -1;
+    const columns = typeof target === "string" ? [target] : target;
+    let str = "",
+      i = -1;
     while (++i < columns.length) {
-      if (i > 0) str += ', '
-      str += prefix + this.wrap(columns[i])
+      if (i > 0) str += ", ";
+      str += prefix + this.wrap(columns[i]);
     }
-    return str
+    return str;
   }
-
 }
 
-export default Client_MSSQL
+export default Client_MSSQL;
--- src/dialects/mssql/transaction.js
+++ src/dialects/mssql/transaction.js
@@ -1,84 +1,95 @@
-import Promise from 'bluebird';
-import Transaction from '../../transaction';
-const debug = require('debug')('knex:tx')
+import Promise from "bluebird";
+import Transaction from "../../transaction";
+const debug = require("debug")("knex:tx");
 
 export default class Transaction_MSSQL extends Transaction {
-
   begin(conn) {
-    debug('%s: begin', this.txid)
-    return conn.tx_.begin()
-      .then(this._resolver, this._rejecter)
+    debug("transaction::begin id=%s", this.txid);
+
+    return new Promise((resolve, reject) => {
+      conn.beginTransaction(err => {
+        if (err) {
+          debug(
+            "transaction::begin error id=%s message=%s",
+            this.txid,
+            err.message
+          );
+          return reject(err);
+        }
+        resolve();
+      });
+
+    }, this.txid).then(this._resolver, this._rejecter);
   }
 
   savepoint(conn) {
-    debug('%s: savepoint at', this.txid)
-    return Promise.resolve()
-      .then(() => this.query(conn, `SAVE TRANSACTION ${this.txid}`))
+    debug("transaction::savepoint id=%s", this.txid);
+
+    return new Promise((resolve, reject) => {
+      conn.saveTransaction(err => {
+        if (err) {
+          debug(
+            "transaction::savepoint id=%s message=%s",
+            this.txid,
+            err.message
+          );
+          return reject(err);
+        }
+
+        resolve();
+      });
+    });
   }
 
   commit(conn, value) {
-    this._completed = true
-    debug('%s: commit', this.txid)
-    return conn.tx_.commit()
-      .then(() => this._resolver(value), this._rejecter)
+    debug("transaction::commit id=%s", this.txid);
+
+    return new Promise((resolve, reject) => {
+      conn.commitTransaction(err => {
+        if (err) {
+          debug(
+            "transaction::commit error id=%s message=%s",
+            this.txid,
+            err.message
+          );
+          return reject(err);
+        }
+
+        this._completed = true;
+        resolve(value);
+      }, this.txid);
+    }).then(() => this._resolver(value), this._rejecter);
   }
 
-  release(conn, value) {
-    return this._resolver(value)
-  }
+  // release(conn, value) {
+  //   return this._resolver(value)
+  // }
 
   rollback(conn, error) {
-    this._completed = true
-    debug('%s: rolling back', this.txid)
-    return conn.tx_.rollback()
-      .then(
-        () => this._rejecter(error),
-        err => {
-          if (error) err.originalError = error;
-          return this._rejecter(err);
+    this._completed = true;
+    debug("transaction::rollback id=%s", this.txid);
+
+    return new Promise((resolve, reject) => {
+      conn.rollbackTransaction(err => {
+        if (err) {
+          debug(
+            "transaction::rollback error id=%s message=%s",
+            this.txid,
+            err.message
+          );
+          err.originalError = err;
         }
-      )
+
+        return this._rejecter(error);
+      }, this.txid)
+        .catch(this._rejecter);
+    });
   }
 
-  rollbackTo(conn, error) {
-    debug('%s: rolling backTo', this.txid)
-    return Promise.resolve()
-      .then(() => this.query(conn, `ROLLBACK TRANSACTION ${this.txid}`, 2, error))
-      .then(() => this._rejecter(error))
-  }
-
-  // Acquire a connection and create a disposer - either using the one passed
-  // via config or getting one off the client. The disposer will be called once
-  // the original promise is marked completed.
-  acquireConnection(config) {
-    const t = this
-    const configConnection = config && config.connection
-    return Promise.try(() => {
-      return (t.outerTx ? t.outerTx.conn : null) ||
-        configConnection ||
-        t.client.acquireConnection();
-    }).tap(function(conn) {
-      if (!t.outerTx) {
-        t.conn = conn
-        conn.tx_ = conn.transaction()
-      }
-    }).disposer(function(conn) {
-      if (t.outerTx) return;
-      if (conn.tx_) {
-        if (!t._completed) {
-          debug('%s: unreleased transaction', t.txid)
-          conn.tx_.rollback();
-        }
-        conn.tx_ = null;
-      }
-      t.conn = null
-      if (!configConnection) {
-        debug('%s: releasing connection', t.txid)
-        t.client.releaseConnection(conn)
-      } else {
-        debug('%s: not releasing external connection', t.txid)
-      }
-    })
-  }
-
+  // rollbackTo(conn, error) {
+  //   // debug('%s: rolling backTo', this.txid)
+  //   // return Promise.resolve()
+  //   //   .then(() => this.query(conn, `ROLLBACK TRANSACTION ${this.txid}`, 2, error))
+  //   //   .then(() => this._rejecter(error))
+  // }
 }
