8dc0a716f95babd48aa873d905dcb6064bcf3b1c
Igor Savin <iselwin@gmail.com>
2018-12-03 23:14:34 +0100

    Fix CWD parameter support for CLI (#2935)
    * Fix cwd support for knex-cli. Add test
    
    * Remove redundant code
    
    * Remove commented out code

---
+++ test/jake/jakelib/knexfile.js
@@ -0,0 +1,32 @@
+#!/usr/bin/env jake
+'use strict';
+/* eslint-disable no-undef */
+/* eslint-disable no-console */
+
+const path = require('path');
+const {
+  assertExec,
+  test,
+} = require('../../jake-util/helpers/migrationtesthelper');
+
+const KNEX = path.normalize(__dirname + '/../../../bin/cli.js');
+
+const taskList = [];
+/* * * TESTS * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+
+test(taskList, 'Run migrations with knexfile passed', (temp) => {
+  console.log(`process ${process.cwd()} dir ${__dirname}`);
+  return assertExec(
+    `node ${KNEX} migrate:latest --knexfile=test/jake-util/knexfile/knexfile.js --knexpath=../knex.js`
+  );
+});
+
+test(taskList, 'resolves knexfile correctly with cwd specified', (temp) => {
+  return assertExec(
+    `node ${KNEX} migrate:latest --cwd=test/jake-util/knexfile --knexfile=knexfile.js`
+  );
+});
+
+module.exports = {
+  taskList,
+};
