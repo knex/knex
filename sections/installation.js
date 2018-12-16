20bc7d760373b2a99d64f6de4e69b98e6e4452e2
Igor Savin <iselwin@gmail.com>
2018-12-16 09:03:08 -0600

    Document custom user params (#135)
    * Document changes in migration config
    
    * Add documentation for initial param setup

--- sections/installation.js
+++ sections/installation.js
@@ -163,6 +163,28 @@ export default [
     `
   },
   {
+    type: "text",
+    content: "`userParams` is an optional parameter that allows you to pass arbitrary parameters which will be accessible via `knex.userParams` property:"
+  },
+  {
+    type: "code",
+    language: "js",
+    content: `
+      var knex = require('knex')({
+        client: 'mysql',
+        connection: {
+          host : '127.0.0.1',
+          user : 'your_database_user',
+          password : 'your_database_password',
+          database : 'myapp_test'
+        },
+        userParams: {
+          userParam1: '451'
+        }
+      });
+    `
+  },
+  {
     type: "info",
     content: "Initializing the library should normally only ever happen once in your application, as it creates a connection pool for the current database, you should use the instance returned from the initialize call throughout your library."
   },
@@ -180,6 +202,28 @@ export default [
 
       pg('table').insert({a: 'b'}).returning('*').toString();
       // "insert into "table" ("a") values ('b') returning *"
+    `
+  },
+  {
+    type: "heading",
+    size: "md",
+    content: "Getting parametrized instance",
+    href: "Installation-parametrization"
+  },
+  {
+    type: "text",
+    content: "You can call method `withUserParams` on a Knex instance if you want to get a copy (with same connections) with custom parameters (e. g. to execute same migrations with different parameters)"
+  },
+  {
+    type: "code",
+    language: "js",
+    content: `
+      var knex = require('knex')({
+        // Params
+      };
+      
+      var knexWithParams = knex.withUserParams({customUserParam: 'table1'});
+      var customUserParam = knexWithParams.userParams.customUserParam;
     `
   },
   {
