9fe4adcd749e969939753b327b5a0cd3d9ed3c28
Zach McElrath <zach@skuidify.com>
2019-04-08 16:15:55 -0400

    Add more detailed example of a migration file (#187)
    * Add more detailed example of a migration file
    
    * Add missing closing braces

--- sections/migrations.js
+++ sections/migrations.js
@@ -247,9 +247,25 @@ export default [
     type: "code",
     language: "js",
     content: `
-      exports.up = function(knex, Promise) { /* ... */ };
+      exports.up = function(knex) {
+        return knex.schema
+          .createTable('user', function (table) {
+             table.increments('id');
+             table.string('first_name', 255).notNullable();
+             table.string('last_name', 255).notNullable();
+          })
+          .createTable('product', function (table) {
+             table.increments('id');
+             table.decimal('price').notNullable();
+             table.string('name', 1000).notNullable();
+          });
+      };
 
-      exports.down = function(knex, Promise) { /* ... */ };
+      exports.down = function(knex) {
+        return knex.schema
+            .dropTable("product")
+            .dropTable("user");
+      };
 
       exports.config = { transaction: false };
     `
