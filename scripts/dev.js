52b6ac19a27113d7e4ad2dfbb87212f70ad81914
Lorefnon <lorefnon@gmail.com>
2019-05-27 05:01:01 +0530

    Fix npm run dev (#196)

--- scripts/dev.js
+++ scripts/dev.js
@@ -66,7 +66,7 @@ portfinder.getPorts(2, {}, (portFinderErr, [docsPort, webpackPort]) => {
 
   runCmd('webpack-dev-server', `node_modules/.bin/nodemon --watch webpack --watch scripts/webpack.config.js --exec webpack-dev-server -- --config scripts/webpack.config.js --color --port ${webpackPort} --debug --hot --host ${ip.address()}`)
 
-  runCmd('docs-server', 'node_modules/.bin/nodemon -- --watch components --watch sections -e js,jsx --exec babel-node scripts/server.js', {
+  runCmd('docs-server', 'node_modules/.bin/nodemon --watch components --watch sections -e js,jsx --exec babel-node scripts/server.js', {
     env: {
       PORT: docsPort,
       WEBPACK_DEV_PORT: webpackPort,
