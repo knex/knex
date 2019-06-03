9b74ccbfad0ccc029220f67e078fcab55a71c0f2
Adrien Risser <risseraka@gmail.com>
2019-06-03 18:10:23 +0200

    Fix error message bubbling up on seed error (#3248)

--- src/seed/Seeder.js
+++ src/seed/Seeder.js
@@ -124,25 +124,26 @@ Seeder.prototype._waterfallBatch = function(seeds) {
     seed = require(name);
 
     // Run each seed file.
-    current = current
-      .then(() => seed.seed(knex, Promise))
-      .then(() => {
-        log.push(name);
-      })
-      .catch((originalError) => {
-        const error = new Error(
-          `Error while executing "${name}" seed: ${originalError.message}`
-        );
-        error.original = originalError;
-        error.stack =
-          error.stack
-            .split('\n')
-            .slice(0, 2)
-            .join('\n') +
-          '\n' +
-          originalError.stack;
-        throw error;
-      });
+    current = current.then(() =>
+      // Nesting promise to prevent bubbling up of error on catch
+      Promise.resolve()
+        .then(() => seed.seed(knex, Promise))
+        .then(() => log.push(name))
+        .catch((originalError) => {
+          const error = new Error(
+            `Error while executing "${name}" seed: ${originalError.message}`
+          );
+          error.original = originalError;
+          error.stack =
+            error.stack
+              .split('\n')
+              .slice(0, 2)
+              .join('\n') +
+            '\n' +
+            originalError.stack;
+          throw error;
+        })
+    );
   });
 
   return current.thenReturn([log]);
