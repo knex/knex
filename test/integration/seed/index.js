10d6001e326cd9e56e1d7983fbd3b53bce178c4a
Felix Mosheev <9304194+felixmosh@users.noreply.github.com>
2019-07-06 19:52:24 +0300

    Refactor to async await

--- test/integration/seed/index.js
+++ test/integration/seed/index.js
@@ -6,31 +6,32 @@ const rimraf = require('rimraf');
 
 module.exports = function(knex) {
   describe('knex.seed.make', () => {
-    it('should create a new seed file with the make method', () => {
-      return knex.seed.make('test').then((name) => {
-        rimraf.sync(path.dirname(name));
-        expect(path.basename(name)).to.equal('test.js');
-      });
+    it('should create a new seed file with the make method', async () => {
+      const name = await knex.seed.make('test');
+
+      rimraf.sync(path.dirname(name));
+      expect(path.basename(name)).to.equal('test.js');
     });
   });
 
   describe('knex.seed.run', () => {
-    it('should run all seed files in the configured seed directory', () => {
-      return knex.seed
-        .run({ directory: 'test/integration/seed/test' })
-        .then(([data]) => {
-          expect(path.basename(data[0])).to.equal('seed1.js');
-          expect(path.basename(data[1])).to.equal('seed2.js');
-        });
+    it('should run all seed files in the configured seed directory', async () => {
+      const [data] = await knex.seed.run({
+        directory: 'test/integration/seed/test',
+      });
+
+      expect(path.basename(data[0])).to.equal('seed1.js');
+      expect(path.basename(data[1])).to.equal('seed2.js');
     });
 
-    it('should run specific seed file in the configured seed directory', () => {
-      return knex.seed
-        .run({ directory: 'test/integration/seed/test', specific: 'seed2.js' })
-        .then(([data]) => {
-          expect(data.length).to.equal(1);
-          expect(path.basename(data[0])).to.equal('seed2.js');
-        });
+    it('should run specific seed file in the configured seed directory', async () => {
+      const [data] = await knex.seed.run({
+        directory: 'test/integration/seed/test',
+        specific: 'seed2.js',
+      });
+
+      expect(data.length).to.equal(1);
+      expect(path.basename(data[0])).to.equal('seed2.js');
     });
   });
 };
