f26db1e0846b5895192575a27e3b9cd7daad679a
Lorefnon <lorefnon@gmail.com>
2019-10-13 16:31:47 +0530

    Fix broken travis badge and links after relocation of project (#240)

--- sections/prelude.js
+++ sections/prelude.js
@@ -5,10 +5,10 @@ export default (version) => [
     content: [
       `<img height="108" id="logo" src="assets/images/knex.png" alt="Knex.js" />`,
       "**Knex.js** is a \"batteries included\" SQL query builder for **Postgres**, **MSSQL**, **MySQL**, **MariaDB**, **SQLite3**, **Oracle**, and **Amazon Redshift** designed to be flexible, portable, and fun to use. It features both traditional node style [callbacks](#Interfaces-Callbacks) as well as a [promise](#Interfaces-Promises) interface for cleaner async flow control, [a stream interface](#Interfaces-Streams), full featured [query](#Builder) and [schema](#Schema) builders, [**transaction support (with savepoints)**](#Transactions), connection [pooling](#Installation-pooling) and standardized responses between different query clients and dialects.",
-      "The project is [hosted on GitHub](http://github.com/tgriesser/knex), and has a comprehensive [test suite](https://travis-ci.org/tgriesser/knex).",
-      "Knex is available for use under the [MIT software license](http://github.com/tgriesser/knex/blob/master/LICENSE).",
-      "You can report bugs and discuss features on the [GitHub issues page](http://github.com/tgriesser/knex/issues), add pages to the [wiki](https://github.com/tgriesser/knex/wiki) or send tweets to [@tgriesser](http://twitter.com/tgriesser).",
-      "Thanks to all of the great [contributions](https://github.com/tgriesser/knex/graphs/contributors) to the project."
+      "The project is [hosted on GitHub](http://github.com/knex/knex), and has a comprehensive [test suite](https://travis-ci.org/knex/knex).",
+      "Knex is available for use under the [MIT software license](http://github.com/knex/knex/blob/master/LICENSE).",
+      "You can report bugs and discuss features on the [GitHub issues page](http://github.com/knex/knex/issues), add pages to the [wiki](https://github.com/knex/knex/wiki) or send tweets to [@tgriesser](http://twitter.com/tgriesser).",
+      "Thanks to all of the great [contributions](https://github.com/knex/knex/graphs/contributors) to the project."
     ]
   },
   {
@@ -22,6 +22,6 @@ export default (version) => [
   },
   {
     type: "text",
-    content: "Current Develop — [![Travis Badge](https://travis-ci.org/tgriesser/knex.png?branch=master)](https://travis-ci.org/tgriesser/knex)"
+    content: "Current Develop — [![Travis Badge](https://travis-ci.org/knex/knex.png?branch=master)](https://travis-ci.org/knex/knex)"
   }
 ]
