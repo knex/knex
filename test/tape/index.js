9d3c543f2a3c0b51bd529d1c0007b37ad8121b28
Oran Epelbaum <oran@avian.aero>
2019-10-26 11:50:47 +0300

    fixed removal of tests from tape

--- test/tape/index.js
+++ test/tape/index.js
@@ -13,7 +13,6 @@ require('./migrate');
 require('./pool');
 require('./knex');
 require('./invalid-db-setup')(knexfile);
-require('./connection-config-provider');
 
 Object.keys(knexfile).forEach(function(key) {
   var knex = makeKnex(knexfile[key]);
