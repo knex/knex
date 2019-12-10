b46c230614b4b7c52ee55cbb65e3312ef2b2b824
Kabir Baidhya <kabirbaidhya@gmail.com>
2019-12-11 00:42:49 +0545

    Add test for sql injection

--- test/unit/query/string.js
+++ test/unit/query/string.js
@@ -129,8 +129,39 @@ describe('String utility functions', () => {
         String.fromCharCode(13) +
         String.fromCharCode(10);
       const output2 = "'" + '\\r\\nFoo\\t\\tBar\\r\\n' + "'";
+
       expect(escapeString(input1)).to.equal(output1);
       expect(escapeString(input2)).to.equal(output2);
+    });
+
+    it('should quote the values well when used in SQL queries', () => {
+      const user = 'foo';
+      const password = "Test;$3`'[/*;";
+      const sql =
+        'SELECT * FROM users WHERE user = ' +
+        escapeString(user) +
+        ' AND password = ' +
+        escapeString(password) +
+        ';';
+      const sqlOutput =
+        "SELECT * FROM users WHERE user = 'foo' AND password = 'Test;$3`\\'[/*;';";
+
+      expect(sql).to.equal(sqlOutput);
+    });
+
+    it('should quote the values subjective to SQL injection too when used in SQL queries', () => {
+      const user = "' OR 0 = 0";
+      const password = "' OR 1 = 1; DROP TABLE users; /*";
+      const sql =
+        'SELECT * FROM users WHERE user = ' +
+        escapeString(user) +
+        ' AND password = ' +
+        escapeString(password) +
+        ';';
+      const sqlOutput =
+        "SELECT * FROM users WHERE user = '\\' OR 0 = 0' AND password = '\\' OR 1 = 1; DROP TABLE users; /*';";
+
+      expect(sql).to.equal(sqlOutput);
     });
   });
 });
