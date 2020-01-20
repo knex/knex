2f4219193e6e07da04e51337e053d01c6c31c47a
saostad <16025625+saostad@users.noreply.github.com>
2020-01-20 17:34:21 -0500

    interface MsSqlConnectionConfig enableArithAbort: null | boolean is required now. (#3629)

--- types/index.d.ts
+++ types/index.d.ts
@@ -1701,6 +1701,7 @@ declare namespace Knex {
       appName?: string;
       abortTransactionOnError?: boolean;
       trustedConnection?: boolean;
+      enableArithAbort?: boolean;
     };
     pool?: {
       min?: number;
