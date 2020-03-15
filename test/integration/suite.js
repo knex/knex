ed14e2f35fefdef4ac1ce683a4e68828db310ab7
Brian Lauber <blauber@arclia.com>
2020-03-15 15:04:20 -0400

    Removed context.initialize(..) method ...
    ... This was a shortcut for calling Client#initializePool(..) .
    But, Client#initializePool(..) really shouldn't be exposed in
    the first place.

--- test/integration/suite.js
+++ test/integration/suite.js
@@ -44,19 +44,4 @@ module.exports = function(knex) {
       });
     });
   });
-
-  describe('knex.initialize', function() {
-    it('should allow initialize the pool with knex.initialize', function() {
-      expect(knex.client.pool).to.equal(undefined);
-      knex.initialize();
-      expect(knex.client.pool.destroyed).to.equal(false);
-      const waitForDestroy = knex.destroy();
-      expect(knex.client.pool.destroyed).to.equal(true);
-      return waitForDestroy.then(() => {
-        expect(knex.client.pool).to.equal(undefined);
-        knex.initialize();
-        expect(knex.client.pool.destroyed).to.equal(false);
-      });
-    });
-  });
 };
