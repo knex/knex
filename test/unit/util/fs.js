cfa2858c6b9ca64496a917a4f06886aeebfb750a
Kabir Baidhya <kabirbaidhya@gmail.com>
2020-03-24 01:18:35 +0545

    Add tests for promisified fs functions

--- test/unit/util/fs.js
+++ test/unit/util/fs.js
@@ -4,6 +4,8 @@ const path = require('path');
 const { expect } = require('chai');
 const {
   stat,
+  readFile,
+  writeFile,
   createTemp,
   ensureDirectoryExists,
 } = require('../../../lib/util/fs');
@@ -23,6 +25,30 @@ describe('FS functions', () => {
       const unexistingPath = path.join(directoryThatExists, 'abc/xyz/123');
 
       expect(stat(unexistingPath)).to.eventually.be.rejected;
+    });
+  });
+
+  describe('readFile', async () => {
+    const tmpPath = await createTemp();
+    const filePath = path.join(tmpPath, `${Date.now()}1.txt`);
+
+    fs.writeFileSync(filePath, 'Hello World!');
+
+    it('should be able to read from the given file path.', async () => {
+      const contents = await readFile(filePath);
+
+      expect(contents.toString()).to.equal('Hello World!');
+    });
+  });
+
+  describe('writeFile', async () => {
+    const tmpPath = await createTemp();
+    const filePath = path.join(tmpPath, `${Date.now()}2.txt`);
+
+    it('should be able to write to the given file path.', async () => {
+      await writeFile(filePath, 'Foo Bar');
+
+      expect(fs.readFileSync(filePath).toString()).to.equal('Foo Bar');
     });
   });
 
