a6551559f07bcb91be7a66fd8d3ae3ede3ba15bf
Kabir Baidhya <kabirbaidhya@gmail.com>
2020-03-24 20:35:47 +0545

    Minor improvements on the usage of fs utilities - unify all the fs functions into the same util/fs to simplify things (#3749)

--- test/unit/util/fs.js
+++ test/unit/util/fs.js
@@ -4,6 +4,9 @@ const path = require('path');
 const { expect } = require('chai');
 const {
   stat,
+  readdir,
+  readFile,
+  writeFile,
   createTemp,
   ensureDirectoryExists,
 } = require('../../../lib/util/fs');
@@ -23,6 +26,51 @@ describe('FS functions', () => {
       const unexistingPath = path.join(directoryThatExists, 'abc/xyz/123');
 
       expect(stat(unexistingPath)).to.eventually.be.rejected;
+    });
+  });
+
+  describe('readFile', async () => {
+    it('should be able to read from the given file path.', async () => {
+      const tmpPath = await createTemp();
+      const filePath = path.join(tmpPath, `${Date.now()}1.txt`);
+
+      fs.writeFileSync(filePath, 'Hello World!');
+
+      const contents = await readFile(filePath);
+
+      expect(contents.toString()).to.equal('Hello World!');
+    });
+  });
+
+  describe('writeFile', async () => {
+    it('should be able to write to the given file path.', async () => {
+      const tmpPath = await createTemp();
+      const filePath = path.join(tmpPath, `${Date.now()}2.txt`);
+
+      await writeFile(filePath, 'Foo Bar');
+
+      expect(fs.readFileSync(filePath).toString()).to.equal('Foo Bar');
+    });
+  });
+
+  describe('readdir', async () => {
+    it('should read a directory and return a list of files under it.', async () => {
+      const directory = await createTemp();
+
+      // Create files under the directory.
+      fs.writeFileSync(path.join(directory, 'testfile1.txt'), 'testfile1');
+      fs.writeFileSync(path.join(directory, 'testfile2.txt'), 'testfile2');
+      fs.writeFileSync(path.join(directory, 'testfile3.txt'), 'testfile3');
+      fs.writeFileSync(path.join(directory, 'testfile4.txt'), 'testfile4');
+
+      const result = await readdir(directory);
+
+      expect(result).to.deep.equal([
+        'testfile1.txt',
+        'testfile2.txt',
+        'testfile3.txt',
+        'testfile4.txt',
+      ]);
     });
   });
 
