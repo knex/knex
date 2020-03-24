41d02ba5c1a3cde7eebb9442e30d4a75abe2df34
Florent Vilmart <364568+flovilmart@users.noreply.github.com>
2020-03-24 12:07:05 -0400

    CLI: adds support for asynchronous knexfile loading (#3748)

---
+++ test/jake-util/knexfile/knexfile_promise.js
@@ -0,0 +1,12 @@
+module.exports = Promise.resolve({
+  client: 'sqlite3',
+  connection: {
+    filename: __dirname + '/../test.sqlite3',
+  },
+  migrations: {
+    directory: __dirname + '/../knexfile_migrations',
+  },
+  seeds: {
+    directory: __dirname + '/../knexfile_seeds',
+  },
+});
