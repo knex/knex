f6896e0534e2269e7f890c4059f4e00aced9d528
Keno Medenbach <k.medenbach@eplas.net>
2020-05-15 10:47:19 +0200

    drop-with-default-constraint test: "default" => "defaultTo"

--- test/integration/migrate/drop-with-default-constraint/01_create.js
+++ test/integration/migrate/drop-with-default-constraint/01_create.js
@@ -1,6 +1,6 @@
 exports.up = (knex) => {
   return knex.schema.createTable('drop_with_default_constraint', (table) => {
-    table.integer('foo').notNullable().default(0);
+    table.integer('foo').notNullable().defaultTo(0);
     table.string('bar').nullable();
   });
 };
