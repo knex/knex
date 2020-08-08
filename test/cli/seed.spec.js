8856e0e6a7c072a92ecabe3acbfedc021db34d98
Daniel <danieliozzi@gmail.com>
2020-08-08 14:04:08 +0200

    esm seeds test: database doesn't exists on CI or wrong 'cwd'

--- test/cli/seed.spec.js
+++ test/cli/seed.spec.js
@@ -58,13 +58,24 @@ describe('seed:run', () => {
     );
   });
 
-  it('runs "esm" files', () => {
+  it('runs "esm" files', async () => {
+    const cwd = path.resolve(__dirname, '../jake-util/knexfile-esm');
+    const { Database } = new require('sqlite3');
+    const db = new Database(path.resolve(cwd, 'test.sqlite3'));
+    await new Promise((resolve, reject) =>
+      db.exec(`create TABLE if not exists xyz (name TEXT);`, (err) => {
+        if (err) reject(err);
+        else resolve();
+      })
+    );
+    await new Promise((resolve) => db.close(() => resolve()));
     return execCommand(
       [
         `node ${KNEX}`,
-        '--esm',
         'seed:run',
-        `--knexfile=test/jake-util/knexfile-esm/knexfile.js`,
+        '--esm',
+        `--cwd=${cwd}`,
+        `--knexfile=./knexfile.js`,
       ].join(' '),
       {
         expectedOutput: 'Ran 1 seed files',
@@ -73,8 +84,17 @@ describe('seed:run', () => {
     );
   });
 
-  it('runs "esm" files from "module"', () => {
+  it('runs "esm" files from "module"', async () => {
     const cwd = path.resolve(__dirname, '../jake-util/knexfile-esm-module');
+    const { Database } = new require('sqlite3');
+    const db = new Database(path.resolve(cwd, 'test.sqlite3'));
+    await new Promise((resolve, reject) =>
+      db.exec(`create TABLE if not exists xyz (name TEXT);`, (err) => {
+        if (err) reject(err);
+        else resolve();
+      })
+    );
+    await new Promise((resolve) => db.close(() => resolve()));
     return execCommand(
       [
         `node ${KNEX}`,
