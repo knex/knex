98c9bb9c7e45e66259e3342575e16c4c7d0b2c34
Eli Geller <elitree@gmail.com>
2020-08-08 10:41:00 -0400

    Typo fix in schema.js (#279)

--- sections/schema.js
+++ sections/schema.js
@@ -356,7 +356,7 @@ export default [
     type: "method",
     method: "timestamps",
     example: "table.timestamps([useTimestamps], [defaultToNow])",
-    description: "Adds created_at and updated_at columns on the database, setting each to datetime types. When true is passed as the first argument a timestamp type is used instead. Both colums default to being not null and using the current timestamp when true is passed as the second argument. Note that on MySQL the .timestamps() only have seconds precision, to get better precision use the .datetime or .timestamp methods directly with precision.",
+    description: "Adds created_at and updated_at columns on the database, setting each to datetime types. When true is passed as the first argument a timestamp type is used instead. Both columns default to being not null and using the current timestamp when true is passed as the second argument. Note that on MySQL the .timestamps() only have seconds precision, to get better precision use the .datetime or .timestamp methods directly with precision.",
     children: [    ]
   },
   {
