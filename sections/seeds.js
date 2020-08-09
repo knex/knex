dcc6301c37419d61ded28af7c32ce2422c4e3166
Mathieu DARTIGUES <mathieu@dartic.fr>
2020-08-09 22:57:32 +0200

    Add array of paths and sortDirsSeparately for seeds (#282)
    related to https://github.com/knex/knex/pull/3967 and https://github.com/knex/documentation/pull/220

--- sections/seeds.js
+++ sections/seeds.js
@@ -15,9 +15,10 @@ export default [
   {
     type: "list",
     content: [
-      "`directory`: a relative path to the directory containing the seed files (default `./seeds`)",
+      "`directory`: a relative path to the directory containing the seed files. Can be an array of paths (default `./seeds`)",
       "`loadExtensions`: array of file extensions which knex will treat as seeds. For example, if you have typescript transpiled into javascript in the same folder, you want to execute only javascript seeds. In this case, set `loadExtensions` to `['.js']`  (Notice the dot!)  (default `['.co', '.coffee', '.eg', '.iced', '.js', '.litcoffee', '.ls', '.ts']`)",
-      "`specific`: a specific seed file to run from the seeds directory, if its value is `undefined` it will run all the seeds (default `undefined`)"
+      "`specific`: a specific seed file to run from the seeds directory, if its value is `undefined` it will run all the seeds (default `undefined`)",
+      "`sortDirsSeparately`: if true and multiple directories are specified, all migrations from a single directory will be executed before executing migrations in the next folder (default `false`)"
     ]
   },
   {
@@ -30,7 +31,7 @@ export default [
     type: "method",
     method: "make",
     example: "knex.seed.make(name, [config])",
-    description: "Creates a new seed file, with the name of the seed file being added.",
+    description: "Creates a new seed file, with the name of the seed file being added. If the seed directory config is an array of paths, the seed file will be generated in the latest specified.",
     children: [    ]
   },
   {
