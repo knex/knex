c5946c783a8b93bb0e6e79f6a59aa869b446e0cd
Kaleb <kaleb.matthews@withone.vision>
2020-10-22 17:09:31 -0400

    Add mssql '?' test

--- test/db-less-test-suite.js
+++ test/db-less-test-suite.js
@@ -27,6 +27,13 @@ describe('Query Building Tests', function () {
 });
 
 const config = require('./knexfile');
+if (config.mssql) {
+  describe('MSSQL driver tests', function () {
+    this.timeout(process.env.KNEX_TEST_TIMEOUT || 5000);
+    require('./unit/dialects/mssql');
+  });
+}
+
 if (config.oracledb) {
   describe('Oracledb driver tests', function () {
     this.timeout(process.env.KNEX_TEST_TIMEOUT || 5000);
