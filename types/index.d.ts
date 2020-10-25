537f4bf2ba6700eb1a686e2ebc2f116c3c97203e
Ivan Zhuravlev <intech@users.noreply.github.com>
2020-10-25 22:03:54 +0300

    Update index.d.ts
    add `Statements` enum in `clear()`

--- types/index.d.ts
+++ types/index.d.ts
@@ -424,6 +424,7 @@ declare namespace Knex {
   //
   // QueryInterface
   //
+  type Statements = "with" | "select" | "columns" | "where" | "union" | "join" | "group" | "order" | "having" | "limit" | "offset";
 
   interface QueryInterface<TRecord extends {} = any, TResult = any> {
     select: Select<TRecord, TResult>;
@@ -533,7 +534,7 @@ declare namespace Knex {
     clearOrder(): QueryBuilder<TRecord, TResult>;
     clearHaving(): QueryBuilder<TRecord, TResult>;
     clearCounters(): QueryBuilder<TRecord, TResult>;
-    clear(statement: string): QueryBuilder<TRecord, TResult>;
+    clear(statement: Statements): QueryBuilder<TRecord, TResult>;
 
     // Paging
     offset(offset: number): QueryBuilder<TRecord, TResult>;
