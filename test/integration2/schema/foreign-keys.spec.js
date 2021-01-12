80ef903b43804793a4fe89addaed17387b89d6f0
Bittersweet Shimmer <bttrswt@protonmail.com>
2021-01-12 22:36:39 +0100

    add tests for SQLite

--- test/integration2/schema/foreign-keys.spec.js
+++ test/integration2/schema/foreign-keys.spec.js
@@ -86,6 +86,58 @@ describe('Schema', () => {
             }
           });
 
+          it('generates correct SQL for the new foreign key operation with an on delete clause', async () => {
+            const builder = knex.schema.alterTable(
+              'foreign_keys_table_one',
+              (table) => {
+                table
+                  .foreign('fkey_three')
+                  .references('foreign_keys_table_three.id')
+                  .withKeyName('fk_fkey_threeee')
+                  .onDelete('CASCADE');
+              }
+            );
+
+            const queries = await builder.generateDdlCommands();
+
+            if (isSQLite(knex)) {
+              expect(queries).to.eql([
+                'CREATE TABLE `_knex_temp_alter111` (`id` integer not null primary key autoincrement, `fkey_two` integer not null, `fkey_three` integer not null)',
+                'INSERT INTO _knex_temp_alter111 SELECT * FROM foreign_keys_table_one;',
+                'DROP TABLE "foreign_keys_table_one"',
+                'CREATE TABLE `foreign_keys_table_one` (`id` integer not null primary key autoincrement, `fkey_two` integer not null, `fkey_three` integer not null, CONSTRAINT fk_fkey_threeee FOREIGN KEY (`fkey_three`)  REFERENCES `foreign_keys_table_three` (`id`) ON DELETE CASCADE)',
+                'INSERT INTO foreign_keys_table_one SELECT * FROM _knex_temp_alter111;',
+                'DROP TABLE "_knex_temp_alter111"',
+              ]);
+            }
+          });
+
+          it('generates correct SQL for the new foreign key operation with an on update clause', async () => {
+            const builder = knex.schema.alterTable(
+              'foreign_keys_table_one',
+              (table) => {
+                table
+                  .foreign('fkey_three')
+                  .references('foreign_keys_table_three.id')
+                  .withKeyName('fk_fkey_threeee')
+                  .onUpdate('CASCADE');
+              }
+            );
+
+            const queries = await builder.generateDdlCommands();
+
+            if (isSQLite(knex)) {
+              expect(queries).to.eql([
+                'CREATE TABLE `_knex_temp_alter111` (`id` integer not null primary key autoincrement, `fkey_two` integer not null, `fkey_three` integer not null)',
+                'INSERT INTO _knex_temp_alter111 SELECT * FROM foreign_keys_table_one;',
+                'DROP TABLE "foreign_keys_table_one"',
+                'CREATE TABLE `foreign_keys_table_one` (`id` integer not null primary key autoincrement, `fkey_two` integer not null, `fkey_three` integer not null, CONSTRAINT fk_fkey_threeee FOREIGN KEY (`fkey_three`)  REFERENCES `foreign_keys_table_three` (`id`) ON UPDATE CASCADE)',
+                'INSERT INTO foreign_keys_table_one SELECT * FROM _knex_temp_alter111;',
+                'DROP TABLE "_knex_temp_alter111"',
+              ]);
+            }
+          });
+
           it('creates new foreign key', async () => {
             await knex('foreign_keys_table_two').insert({});
             await knex('foreign_keys_table_three').insert({});
