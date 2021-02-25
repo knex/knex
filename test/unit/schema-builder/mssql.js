7db2d188773a955f040252ba33a0e1ae59e2575f
Daniel Hensby <dhensby@users.noreply.github.com>
2021-02-25 17:34:22 +0000

    fix mssql alter column must have its own query (#4317)

--- test/unit/schema-builder/mssql.js
+++ test/unit/schema-builder/mssql.js
@@ -165,7 +165,25 @@ describe('MSSQL SchemaBuilder', function () {
       'ALTER TABLE [users] ADD [bar] nvarchar(255)'
     );
     expect(tableSql[1].sql).to.equal(
-      'ALTER TABLE [users] alter column [foo] nvarchar(255)'
+      'ALTER TABLE [users] ALTER COLUMN [foo] nvarchar(255)'
+    );
+  });
+
+  it('should alter multiple columns over multiple queries', function () {
+    tableSql = client
+      .schemaBuilder()
+      .table('users', function () {
+        this.string('foo').alter();
+        this.string('bar').alter();
+      })
+      .toSQL();
+
+    equal(2, tableSql.length);
+    expect(tableSql[0].sql).to.equal(
+      'ALTER TABLE [users] ALTER COLUMN [foo] nvarchar(255)'
+    );
+    expect(tableSql[1].sql).to.equal(
+      'ALTER TABLE [users] ALTER COLUMN [bar] nvarchar(255)'
     );
   });
 
