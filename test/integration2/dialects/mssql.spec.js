16b5c87c78b4cca50c179332a8427df4dd526fc1
Jeremy W. Sherman <jeremyw.sherman@gmail.com>
2021-03-17 15:15:50 -0400

    mssql: Verify 7500 byte comment length limit

--- test/integration2/dialects/mssql.spec.js
+++ test/integration2/dialects/mssql.spec.js
@@ -87,6 +87,73 @@ describe('MSSQL dialect', () => {
             expect(name).to.match(/^DF__test__name__[0-9A-Z]+$/);
           });
         });
+
+        describe('comment limits', () => {
+          // NOTE: We are using varchar, not nvarchar, so we can hit an odd number of bytes with our characters.
+          const byteCount0 = "''";
+          const byteCount7500 = "'" + 'X'.repeat(7500) + "'";
+          const byteCountOver7500 = "'" + 'X'.repeat(7501) + "'";
+
+          const tableTarget = { schemaName: 'dbo', tableName: 'test' };
+          it('supports table comments of max 7500 bytes', async () => {
+            // 0 works
+            await expect(comment(knex, 'add', byteCount0, tableTarget)).to
+              .eventually.be.fulfilled;
+
+            // 7500 works
+            await expect(comment(knex, 'update', byteCount7500, tableTarget)).to
+              .eventually.be.fulfilled;
+
+            // Over 7500 fails
+            await expect(
+              comment(knex, 'update', byteCountOver7500, tableTarget)
+            ).to.eventually.be.rejectedWith(
+              'The size associated with an extended property cannot be more than 7,500 bytes.'
+            );
+          });
+
+          const columnTarget = { ...tableTarget, columnName: 'id' };
+          it('supports column comments of max 7500 bytes', async () => {
+            // 0 works
+            await expect(comment(knex, 'add', byteCount0, columnTarget)).to
+              .eventually.be.fulfilled;
+
+            // 7500 works
+            await expect(comment(knex, 'update', byteCount7500, columnTarget))
+              .to.eventually.be.fulfilled;
+
+            // Over 7500 fails
+            await expect(
+              comment(knex, 'update', byteCountOver7500, columnTarget)
+            ).to.eventually.be.rejectedWith(
+              'The size associated with an extended property cannot be more than 7,500 bytes.'
+            );
+          });
+        });
       });
     });
 });
+
+/**
+ *
+ * @param {Knex} knex
+ * @param {'add' | 'update' | 'drop'} action
+ * @param {string} rawQuotedComment No escaping is done. This should include any needed quote marks.
+ * @param {{schemaName: string, tableName: string, columnName?: string}} target
+ */
+const comment = async (knex, action, rawQuotedComment, target) => {
+  const { schemaName, tableName } = target;
+  // NOTE: This does not escape any embedded single quotes!
+  // (Since we control all the inputs here, we know we don't need to.)
+  const lines = [
+    `EXEC sp_${action}extendedproperty`,
+    " @name = N'MS_Description'",
+    action !== 'drop' ? `, @value = ${rawQuotedComment}` : '',
+    `, @level0type = N'Schema', @level0name = N'${schemaName}'`,
+    `, @level1type = N'Table', @level1name = N'${tableName}'`,
+    target.columnName !== undefined
+      ? `, @level2type = N'Column', @level2name = N'${target.columnName}'`
+      : '',
+  ];
+  return await knex.schema.raw(lines.join(''));
+};
