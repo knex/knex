e513d5125290f586c1d074e07d0cfbcc12f02845
Jonathan <barroudjo@gmail.com>
2021-05-11 22:42:36 +0200

    fixed typescript example of extending QueryBuilder (#321)

--- sections/builder.js
+++ sections/builder.js
@@ -2811,11 +2811,14 @@ export default [
     content: `
       // knex.d.ts
 
-      import { Knex } from 'knex';
+      import { Knex as KnexOriginal } from 'knex';
 
       declare module 'knex' {
-        interface QueryBuilder {
-        customSelect<TRecord, TResult>(value: number): Knex.QueryBuilder<TRecord, TResult>;
+        namespace Knex {
+          interface QueryBuilder {
+            customSelect<TRecord, TResult>(value: number): KnexOriginal.QueryBuilder<TRecord, TResult>;
+          }
+        }
       }
     `
   },
