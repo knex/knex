c778c82f2fd39944e081effa7ce945438cb62ba8
Aiden Foxx <aiden.foxx.mail@gmail.com>
2021-09-19 17:14:06 +0200

    Added setNullable support to SQLite (#4684)

--- test/integration2/schema/set-nullable.spec.js
+++ test/integration2/schema/set-nullable.spec.js
@@ -1,6 +1,7 @@
 const chai = require('chai');
 chai.use(require('chai-as-promised'));
 const expect = chai.expect;
+const sinon = require('sinon');
 const {
   isSQLite,
   isPostgreSQL,
@@ -16,13 +17,12 @@ describe('Schema', () => {
         let knex;
 
         before(function () {
+          sinon.stub(Math, 'random').returns(0.1);
           knex = getKnexForDb(db);
-          if (isSQLite(knex)) {
-            return this.skip();
-          }
         });
 
         after(() => {
+          sinon.restore();
           return knex.destroy();
         });
 
@@ -38,6 +38,31 @@ describe('Schema', () => {
         });
 
         describe('setNullable', () => {
+          it('should generate correct SQL for set nullable operation', async () => {
+            const builder = knex.schema.table('primary_table', (table) => {
+              table.setNullable('id_not_nullable');
+            });
+            const queries = await builder.generateDdlCommands();
+
+            if (isSQLite(knex)) {
+              expect(queries.sql).to.eql([
+                'CREATE TABLE `_knex_temp_alter111` (`id_nullable` integer NULL, `id_not_nullable` integer)',
+                'INSERT INTO _knex_temp_alter111 SELECT * FROM primary_table;',
+                'DROP TABLE "primary_table"',
+                'ALTER TABLE "_knex_temp_alter111" RENAME TO "primary_table"',
+              ]);
+            }
+
+            if (isPostgreSQL(knex)) {
+              expect(queries.sql).to.eql([
+                {
+                  bindings: [],
+                  sql: 'alter table "primary_table" alter column "id_not_nullable" drop not null',
+                },
+              ]);
+            }
+          });
+
           it('sets column to be nullable', async () => {
             await knex.schema.table('primary_table', (table) => {
               table.setNullable('id_not_nullable');
@@ -63,6 +88,9 @@ describe('Schema', () => {
               errorMessage = 'cannot be null';
             } else if (isOracle(knex)) {
               errorMessage = 'ORA-01400: cannot insert NULL into';
+            } else if (isSQLite(knex)) {
+              errorMessage =
+                'insert into `primary_table` (`id_not_nullable`, `id_nullable`) values (1, NULL) - SQLITE_CONSTRAINT: NOT NULL constraint failed: primary_table.id_nullable';
             }
 
             await expect(
