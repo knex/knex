e84249c2838cf052354e90c159d765304220304a
Flleeppyy <18307183+flleeppyy@users.noreply.github.com>
2021-10-11 12:20:13 -0700

    Missing comma (#340)

--- sections/installation.js
+++ sections/installation.js
@@ -120,7 +120,7 @@ export default [
       const knex = require('knex')({
         client: 'sqlite3',
         connection: {
-          filename: "file:memDb1?mode=memory&cache=shared"
+          filename: "file:memDb1?mode=memory&cache=shared",
           flags: ['OPEN_URI', 'OPEN_SHAREDCACHE']
         }
       });
