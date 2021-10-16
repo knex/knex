99d2015afde707c1ab1d363ca9af3cdc21809344
Marceli Wac <marceliwac@gmail.com>
2021-10-16 18:05:57 +0100

    chore: Refactor existing tests to be part of the right suite.

--- test/unit/migrations/migrate/migration-list-resolver.js
+++ test/unit/migrations/migrate/migration-list-resolver.js
@@ -93,6 +93,23 @@ describe('migration-list-resolver', () => {
           ]);
         });
     });
+
+    it('should pass loadExtensions param to listAll', () => {
+      after(() => {
+        sinon.restore();
+      });
+
+      const migrationSource = new FsMigrations([], true);
+
+      const stub = sinon
+        .stub(migrationSource, 'getMigrations')
+        .callsFake(async () => true);
+      return migrationListResolver
+        .listAll(migrationSource, ['.ts'])
+        .then(() => {
+          sinon.assert.calledWith(stub, ['.ts']);
+        });
+    });
   });
 
   describe('listAll - multiple directories', () => {
@@ -189,25 +206,6 @@ describe('migration-list-resolver', () => {
               file: '004_migration.js',
             },
           ]);
-        });
-    });
-  });
-
-  describe('listAllAndCompleted', () => {
-    it('should pass loadExtensions param to listAll', () => {
-      after(() => {
-        sinon.restore();
-      });
-
-      const migrationSource = new FsMigrations([], true);
-
-      const stub = sinon
-        .stub(migrationSource, 'getMigrations')
-        .callsFake(async () => true);
-      return migrationListResolver
-        .listAll(migrationSource, ['.ts'])
-        .then(() => {
-          sinon.assert.calledWith(stub, ['.ts']);
         });
     });
   });
