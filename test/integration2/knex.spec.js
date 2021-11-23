9240a4b87f14b961fb63a9d6bd2c32dd2dfc718b
Mehdi Saffar <saffar.mehdi3@gmail.com>
2021-11-23 11:48:05 +0300

    add integration test

--- test/integration2/knex.spec.js
+++ test/integration2/knex.spec.js
@@ -1,3 +1,4 @@
+const { expect } = require('chai');
 const { getAllDbs, getKnexForDb } = require('./util/knex-instance-provider');
 
 describe('knex', () => {
@@ -18,6 +19,61 @@ describe('knex', () => {
           await knex.destroy();
         });
       });
+
+      if (db === 'postgres') {
+        describe('initialize', () => {
+          it('actually uses provided new config', async () => {
+            const q1 = await knex.raw('select "current_user"();');
+            expect(q1.rows[0][0]).to.equal('postgres');
+
+            // create new role
+            const user = 'user2';
+            const password = 'user2';
+
+            await createRoleIfNotExists(knex, { user, password });
+
+            // prepare new config based off the current one
+            const oldConfig = knex.client.config;
+            const newConfig = {
+              ...oldConfig,
+              connection: {
+                ...oldConfig.connection,
+                user,
+                password,
+              },
+            };
+
+            // switch role
+            await knex.destroy();
+            knex.initialize(newConfig);
+
+            const q2 = await knex.raw('select "current_user"();');
+            expect(q2.rows[0][0]).to.equal('user2');
+
+            // cleanup
+            await knex.destroy();
+            await knex.initialize(oldConfig);
+            await dropRole(knex, { user });
+          });
+        });
+      }
     });
   });
 });
+
+async function createRoleIfNotExists(knex, { user, password }) {
+  try {
+    return await knex.raw(
+      `CREATE ROLE ${user} WITH LOGIN PASSWORD '${password}';`
+    );
+  } catch (error) {
+    if (error.code !== '42710') {
+      // role already exists
+      throw error;
+    }
+  }
+}
+
+async function dropRole(knex, { user }) {
+  return await knex.raw(`DROP ROLE ${user}`);
+}
