248a9e4be04013854b291e794f0072a82fe5e1ce
Olivier Cavadenti <olivier.cavadenti@gmail.com>
2021-12-09 15:24:13 +0100

    Add useCamelCase doc (#358)

--- sections/schema.js
+++ sections/schema.js
@@ -612,15 +612,15 @@ export default [
   {
     type: "method",
     method: "timestamps",
-    example: "table.timestamps([useTimestamps], [defaultToNow])",
-    description: "Adds created_at and updated_at columns on the database, setting each to datetime types. When true is passed as the first argument a timestamp type is used instead. Both columns default to being not null and using the current timestamp when true is passed as the second argument. Note that on MySQL the .timestamps() only have seconds precision, to get better precision use the .datetime or .timestamp methods directly with precision.",
+    example: "table.timestamps([useTimestamps], [defaultToNow], [useCamelCase])",
+    description: "Adds created_at and updated_at columns on the database, setting each to datetime types. When true is passed as the first argument a timestamp type is used instead. Both columns default to being not null and using the current timestamp when true is passed as the second argument. Note that on MySQL the .timestamps() only have seconds precision, to get better precision use the .datetime or .timestamp methods directly with precision. If useCamelCase is true, the name of columns are createdAt and updatedAt.",
     children: [    ]
   },
   {
     type: "method",
     method: "dropTimestamps",
-    example: "table.dropTimestamps()",
-    description: "Drops the columns created_at and updated_at from the table, which can be created via timestamps.",
+    example: "table.dropTimestamps([useCamelCase])",
+    description: "Drops the columns created_at and updated_at from the table, which can be created via timestamps. If useCamelCase is true, the name of columns are createdAt and updatedAt.",
     children: [    ]
   },
   {
