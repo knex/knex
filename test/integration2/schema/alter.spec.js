e2a425e4e3a82866e5d7c03d46e883f06a9c9245
Nicola Krumschmidt <nicola.krumschmidt@freenet.de>
2021-12-15 19:49:53 +0100

    Revert "Fix SQLite not doing rollback when altering columns fails (#4336)" (#4896)

--- test/integration2/schema/alter.spec.js
+++ test/integration2/schema/alter.spec.js
@@ -170,7 +170,7 @@ describe('Schema', () => {
               );
             });
 
-            it.skip('generates correct SQL commands when altering columns', async () => {
+            it('generates correct SQL commands when altering columns', async () => {
               const builder = knex.schema.alterTable('alter_table', (table) => {
                 table.string('column_integer').alter();
               });
@@ -180,8 +180,8 @@ describe('Schema', () => {
               expect(queries.sql).to.deep.equal([
                 "CREATE TABLE `_knex_temp_alter111` (`column_integer` varchar(255), `column_string` varchar(255), `column_datetime` datetime, `column_defaultTo` integer DEFAULT '0', `column_notNullable` varchar(255) NOT NULL, `column_defaultToAndNotNullable` datetime NOT NULL DEFAULT '0', `column_nullable` boolean NULL)",
                 'INSERT INTO _knex_temp_alter111 SELECT * FROM alter_table;',
-                'DROP TABLE "alter_table"',
-                'ALTER TABLE "_knex_temp_alter111" RENAME TO "alter_table"',
+                "DROP TABLE 'alter_table'",
+                "ALTER TABLE '_knex_temp_alter111' RENAME TO 'alter_table'",
               ]);
             });
           });
