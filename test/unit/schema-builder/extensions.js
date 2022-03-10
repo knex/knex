e2b41fca792efc6252719f60e14c7e290a0bbbff
Dominic Vonk <dominic@vonk.io>
2022-03-10 11:21:01 +0100

    Improved further

--- test/unit/schema-builder/extensions.js
+++ test/unit/schema-builder/extensions.js
@@ -7,19 +7,45 @@ describe('SchemaBuilder Extensions', () => {
   let client = new MySQL_Client({ client: 'mysql' });
 
   const equal = require('assert').equal;
+  const functionToExtend = () => true;
+  knex.SchemaBuilder.extend('tryCreateTable', functionToExtend);
+  knex.TableBuilder.extend('testCol', functionToExtend);
+  knex.ColumnBuilder.extend('testCol', functionToExtend);
+  knex.ViewBuilder.extend('testCol', functionToExtend);
 
-  it('has method', () => {
-    const functionToExtend = () => true;
-    knex.SchemaBuilder.extend('tryCreateTable', functionToExtend);
-    knex.TableBuilder.extend('testCol', functionToExtend);
-    knex.ColumnBuilder.extend('testCol', functionToExtend);
+  it('schemabuilder has method', () => {
     equal(client.schemaBuilder().tryCreateTable, functionToExtend);
     expect(client.schemaBuilder().tryCreateTable()).to.be.true;
+  });
+
+  it('tablebuilder has method', () => {
     client
       .schemaBuilder()
       .createTable('users', function (table) {
         equal(table.testCol, functionToExtend);
+      })
+      .toSQL();
+  });
+
+  it('columnbuilder has method', () => {
+    client
+      .schemaBuilder()
+      .createTable('users', function (table) {
         equal(table.string('t').testCol, functionToExtend);
+      })
+      .toSQL();
+  });
+
+  it('viewbuilder has method', () => {
+    client
+      .schemaBuilder()
+      .createView('users', function (view) {
+        equal(view.testCol, functionToExtend);
+        view.as(
+          knex('users')
+            .join('user_roles', 'users.user_id', 'user_roles.user_id')
+            .select('users.*', 'user_roles.role_code')
+        );
       })
       .toSQL();
   });
