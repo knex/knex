47b329934f51814a451a5ee486b502cb9f4d3d3e
Dominic Vonk <dominic@vonk.io>
2022-03-10 11:10:37 +0100

    Improve unit tests

--- test/unit/schema-builder/extensions.js
+++ test/unit/schema-builder/extensions.js
@@ -11,7 +11,16 @@ describe('SchemaBuilder Extensions', () => {
   it('has method', () => {
     const functionToExtend = () => true;
     knex.SchemaBuilder.extend('tryCreateTable', functionToExtend);
+    knex.TableBuilder.extend('testCol', functionToExtend);
+    knex.ColumnBuilder.extend('testCol', functionToExtend);
     equal(client.schemaBuilder().tryCreateTable, functionToExtend);
     expect(client.schemaBuilder().tryCreateTable()).to.be.true;
+    client
+      .schemaBuilder()
+      .createTable('users', function (table) {
+        equal(table.testCol, functionToExtend);
+        equal(table.string('t').testCol, functionToExtend);
+      })
+      .toSQL();
   });
 });
