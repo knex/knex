867b7712824bdaade2bab6a86e92f57eb0b3b1f7
Jürg Lehni <juerg@scratchdisk.com>
2022-03-18 19:22:56 +0100

    Remove unused process.npm_package_* emulation

--- test/unit/migrations/util/is-module-type.js
+++ test/unit/migrations/util/is-module-type.js
@@ -5,22 +5,6 @@ const isModuleType = require('../../../../lib/migrations/util/is-module-type.js'
 require('../../../util/chai-setup');
 
 describe('isModuleType', () => {
-  let originalEnv = {};
-
-  before(() => {
-    originalEnv = { ...process.env };
-    process.env = {};
-  });
-
-  after(() => {
-    process.env = { ...originalEnv };
-  });
-
-  beforeEach(() => {
-    delete process.env.npm_package_type;
-    delete process.env.npm_package_json;
-  });
-
   const getFile = (package, filename) =>
     path.resolve(__dirname, `test/${package}/${filename}`);
 
@@ -38,15 +22,5 @@ describe('isModuleType', () => {
 
   it('should return false with type=module and a .cjs file', async () => {
     expect(await isModuleType(getFile('module', 'test.cjs'))).to.be.false;
-  });
-
-  it('should return true if type=module with npm < 7.0.0', async () => {
-    process.env.npm_package_type = 'module';
-    expect(await isModuleType(getFile('module', 'test.js'))).to.be.true;
-  });
-
-  it('should return false if type=commonjs with npm < 7.0.0', async () => {
-    process.env.npm_package_type = 'commonjs';
-    expect(await isModuleType(getFile('commonjs', 'test.js'))).to.be.false;
   });
 });
