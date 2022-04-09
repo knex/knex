7bae8eac3f1df3ff9a112e06243493be7f96162d
Olivier Cavadenti <olivier.cavadenti@gmail.com>
2022-04-09 15:50:42 +0200

    update integration test

--- test/integration2/knex.spec.js
+++ test/integration2/knex.spec.js
@@ -1,15 +1,23 @@
 const { expect } = require('chai');
 const { getAllDbs, getKnexForDb } = require('./util/knex-instance-provider');
+const {
+  isPostgreSQL,
+  isMysql,
+  isMssql,
+  isCockroachDB,
+  isSQLite,
+  isPgBasedDriverName,
+} = require('../util/db-helpers');
 
 describe('knex', () => {
   getAllDbs().forEach((db) => {
-    describe(db, () => {
+    describe.only(db, () => {
       let knex;
-      before(() => {
+      beforeEach(() => {
         knex = getKnexForDb(db);
       });
 
-      after(() => {
+      afterEach(() => {
         return knex.destroy();
       });
 
@@ -20,48 +28,78 @@ describe('knex', () => {
         });
       });
 
-      if (db === 'postgres') {
-        describe('initialize', () => {
-          it('actually uses provided new config', async () => {
-            const q1 = await knex.raw('select "current_user"();');
-            expect(q1.rows[0][0]).to.equal('postgres');
-
-            // create new role
-            const user = 'user2';
-            const password = 'user2';
-
-            await createRoleIfNotExists(knex, { user, password });
-
-            // prepare new config based off the current one
-            const oldConfig = knex.client.config;
-            const newConfig = {
-              ...oldConfig,
-              connection: {
-                ...oldConfig.connection,
-                user,
-                password,
-              },
-            };
-
-            // switch role
-            await knex.destroy();
-            knex.initialize(newConfig);
-
-            const q2 = await knex.raw('select "current_user"();');
-            expect(q2.rows[0][0]).to.equal('user2');
-
-            // cleanup
-            await knex.destroy();
-            await knex.initialize(oldConfig);
-            await dropRole(knex, { user });
-          });
+      describe('initialize', function () {
+        beforeEach(async function () {
+          if (!isPostgreSQL(knex)) {
+            this.skip();
+          }
+          await dropRole(knex, 'user2');
         });
-      }
+
+        it('actually uses provided new config', async function () {
+          let currentUserQuery = '';
+
+          // at this time only work on Postgres because of access denied in
+          // all other databases when creating roles.
+          // TODO : fix this for all other database.
+          if (!isPostgreSQL(knex)) {
+            this.skip();
+          }
+          if (isMysql(knex)) {
+            currentUserQuery = 'select CURRENT_USER();';
+          } else if (isMssql(knex)) {
+            currentUserQuery = 'select SYSTEM_USER';
+          } else {
+            currentUserQuery = 'select "current_user"();';
+          }
+
+          const q1 = await knex.raw(currentUserQuery);
+
+          let userName;
+
+          if (isMysql(knex)) {
+            userName = q1[0][0]['CURRENT_USER()'].slice(0, -2);
+          } else if (isMssql(knex)) {
+            userName = Object.values(q1[0])[0];
+          } else {
+            userName = q1.rows[0].current_user;
+          }
+
+          expect(userName).to.equal(knex.client.config.connection.user);
+          // create new role
+          const user = 'user2';
+          const password = 'user2';
+
+          await createRoleIfNotExists(knex, user, password);
+
+          // prepare new config based off the current one
+          const oldConfig = knex.client.config;
+          const newConfig = {
+            ...oldConfig,
+            connection: {
+              ...oldConfig.connection,
+              user,
+              password,
+            },
+          };
+
+          // switch role
+          await knex.destroy();
+          knex.initialize(newConfig);
+
+          const q2 = await knex.raw(currentUserQuery);
+          expect(q2.rows[0].current_user).to.equal('user2');
+
+          // cleanup
+          await knex.destroy();
+          await knex.initialize(oldConfig);
+        });
+      });
     });
   });
 });
 
-async function createRoleIfNotExists(knex, { user, password }) {
+async function createRoleIfNotExists(knex, user, password) {
   try {
     return await knex.raw(
       `CREATE ROLE ${user} WITH LOGIN PASSWORD '${password}';`
@@ -74,6 +112,6 @@ async function createRoleIfNotExists(knex, { user, password }) {
   }
 }
 
-async function dropRole(knex, { user }) {
-  return await knex.raw(`DROP ROLE ${user}`);
+async function dropRole(knex, user) {
+  return await knex.raw(`DROP ROLE IF EXISTS ${user}`);
 }
