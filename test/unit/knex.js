b8fc16f86734913fefe076848b53eb07d0af77c9
Olivier Cavadenti <olivier.cavadenti@gmail.com>
2022-04-21 19:05:40 +0200

    Restore sqlite3 package (#5136)

--- test/unit/knex.js
+++ test/unit/knex.js
@@ -2,7 +2,7 @@ const Knex = require('../../lib/index');
 const QueryBuilder = require('../../lib/query/querybuilder');
 const { expect } = require('chai');
 const sqliteConfig = require('../knexfile').sqlite3;
-const sqlite3 = require('@vscode/sqlite3');
+const sqlite3 = require('sqlite3');
 const { noop } = require('lodash');
 const sinon = require('sinon');
 
@@ -357,8 +357,7 @@ describe('knex', () => {
     ClientFoobar.prototype._driver = () => {
       throw new Error('Cannot require...');
     };
-    ClientFoobar.prototype.driverName = ClientFoobar.prototype.aliasDriverName =
-      'foo-bar';
+    ClientFoobar.prototype.driverName = 'foo-bar';
 
     expect(() => {
       Knex({ client: ClientFoobar, connection: {} });
