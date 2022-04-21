f40f919b5a1045c67c73da2e7a1b592fde4af84e
Joel Venable <30368779+JoelVenable@users.noreply.github.com>
2022-04-21 07:15:31 -0500

    Update type definitions for pg connection

--- types/index.d.ts
+++ types/index.d.ts
@@ -2570,17 +2570,22 @@ export declare namespace Knex {
   interface PgConnectionConfig {
     user?: string;
     database?: string;
-    password?: string;
+    password?: string | (() => string | Promise<string>);
     port?: number;
     host?: string;
     connectionString?: string;
     keepAlive?: boolean;
     stream?: stream.Duplex;
     statement_timeout?: false | number;
-    connectionTimeoutMillis?: number;
-    keepAliveInitialDelayMillis?: number;
+    parseInputDatesAsUTC?: boolean;
     ssl?: boolean | ConnectionOptions;
+    query_timeout?: number;
+    keepAliveInitialDelayMillis?: number;
+    idle_in_transaction_session_timeout?: number;
     application_name?: string;
+    connectionTimeoutMillis?: number;
+    types?: CustomTypesConfig;
+    options?: string;
   }
 
   type RedshiftConnectionConfig = PgConnectionConfig;
