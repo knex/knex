1afb31da6ab8734f6005fdd7e3e6dec0a879267f
Stanley Masinde <stanmasinde@gmail.com>
2022-04-25 04:19:22 +0300

    chore(tests): added one more test to the feature

--- test/cli/schema-dump.spec.js
+++ test/cli/schema-dump.spec.js
@@ -2,13 +2,25 @@
 
 const path = require('path');
 const { execCommand } = require('cli-testlab');
+const { unlinkSync } = require('fs');
 
 const KNEX = path.normalize(__dirname + '/../../bin/cli.js');
 
 describe('schema:dump', () => {
-  it('Dump schema', () => {
+  after(() => {
+    unlinkSync('./test/cli/schema-dump.sql');
+  })
+
+  it('Dumps schema', () => {
+    console.log(KNEX);
     return execCommand(`node ${KNEX} schema:dump`, {
       expectedOutput: { expectedText: 'Dumping schema', exactlyTimes: 1 },
     });
   });
+
+  it('With output option', () => {
+    return execCommand(`node ${KNEX} schema:dump --output=./test/cli/schema-dump.sql`, {
+      expectedOutput: { expectedText: 'Dumping schema', exactlyTimes: 1 },
+    });
+  })
 });
