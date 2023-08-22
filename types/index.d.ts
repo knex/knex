8be73530e9189dc73bbf456a14aa03eb8cd43b1e
roggervalf <rogger.valverde@uni.pe>
2023-08-21 22:00:34 -0500

    chore: add type change

--- types/index.d.ts
+++ types/index.d.ts
@@ -2325,6 +2325,7 @@ declare namespace Knex {
     executionPromise: Promise<TResult>;
     parentTransaction?: Transaction;
     isCompleted: () => boolean;
+    addFunctionOnAfterTransaction: (cb: Promise<void>) => void;
 
     query<TRecord extends {} = any, TResult = void>(
       conn: any,
