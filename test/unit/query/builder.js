04642dab63f0e44ddfaf8e1a4dc49ba929c9211e
Alex Vershinin <alexander.vershinin@quantori.com>
2023-11-28 04:14:36 +0400

    MySQL dialect: added implementation for upsert  (#5743)
    Co-authored-by: Igor Savin <iselwin@gmail.com>

--- test/unit/query/builder.js
+++ test/unit/query/builder.js
@@ -11626,4 +11626,165 @@ describe('QueryBuilder', () => {
       });
     });
   });
+
+  describe('Upsert', () => {
+    function mysqlQb() {
+      return clients.mysql.queryBuilder();
+    }
+
+    const mysqlClient = {
+      mysql: clients.mysql,
+    };
+
+    const mysqlClientWithNullAsDefault = {
+      mysql: clientsWithNullAsDefault.mysql,
+    };
+
+    describe('toSQL method', () => {
+      it('upsert: one record with one field', () => {
+        testsql(
+          mysqlQb().from('users').upsert({ email: 'foo' }),
+          {
+            mysql: {
+              sql: 'replace into `users` (`email`) values (?)',
+              bindings: ['foo'],
+            },
+          },
+          mysqlClient
+        );
+      });
+
+      it('upsert: multiple records with multiple fields', () => {
+        testsql(
+          mysqlQb()
+            .from('users')
+            .upsert([
+              { email: 'foo', name: 'taylor' },
+              { email: 'bar', name: 'dayle' },
+            ]),
+          {
+            mysql: {
+              sql: 'replace into `users` (`email`, `name`) values (?, ?), (?, ?)',
+              bindings: ['foo', 'taylor', 'bar', 'dayle'],
+            },
+          },
+          mysqlClient
+        );
+      });
+
+      it('upsert: one record with returning', () => {
+        // check that returning doesn't break anything
+        testsql(
+          mysqlQb()
+            .from('users')
+            .upsert({ email: 'foo', name: 'taylor' }, 'id'),
+          {
+            mysql: {
+              sql: 'replace into `users` (`email`, `name`) values (?, ?)',
+              bindings: ['foo', 'taylor'],
+            },
+          },
+          mysqlClient
+        );
+      });
+
+      it('upsert: one record with multiple returnings', () => {
+        // check that returning doesn't break anything
+        testsql(
+          mysqlQb()
+            .from('users')
+            .upsert({ email: 'foo', name: 'taylor' }, ['id', 'name']),
+          {
+            mysql: {
+              sql: 'replace into `users` (`email`, `name`) values (?, ?)',
+              bindings: ['foo', 'taylor'],
+            },
+          },
+          mysqlClient
+        );
+      });
+
+      it('upsert: respects raw bindings', () => {
+        testsql(
+          mysqlQb()
+            .from('users')
+            .upsert({ email: raw('CURRENT TIMESTAMP'), name: 'taylor' }),
+          {
+            mysql: {
+              sql: 'replace into `users` (`email`, `name`) values (CURRENT TIMESTAMP, ?)',
+              bindings: ['taylor'],
+            },
+          },
+          mysqlClient
+        );
+      });
+
+      it('upsert: empty query', () => {
+        testsql(
+          mysqlQb().from('users').upsert(),
+          {
+            mysql: {
+              sql: '',
+              bindings: [],
+            },
+          },
+          mysqlClient
+        );
+      });
+
+      it('upsert: empty array', () => {
+        testsql(
+          mysqlQb().from('users').upsert([]),
+          {
+            mysql: {
+              sql: '',
+              bindings: [],
+            },
+          },
+          mysqlClient
+        );
+      });
+
+      it('upsert: empty object in an array', () => {
+        testsql(
+          mysqlQb().from('users').upsert([{}]),
+          {
+            mysql: {
+              sql: 'replace into `users` () values ()',
+              bindings: [],
+            },
+          },
+          mysqlClient
+        );
+      });
+    });
+
+    describe('toQuery method', () => {
+      it('upsert: multiple records with missing fields and no nullAsDefault setting', () => {
+        testquery(
+          mysqlQb()
+            .from('users')
+            .upsert([{ email: 'foo' }, { name: 'dayle' }]),
+          {
+            mysql:
+              "replace into `users` (`email`, `name`) values ('foo', DEFAULT), (DEFAULT, 'dayle')",
+          },
+          mysqlClient
+        );
+      });
+
+      it('upsert: multiple records with missing fields and nullAsDefault setting is true', () => {
+        testquery(
+          mysqlQb()
+            .from('users')
+            .upsert([{ name: 'taylor' }, { email: 'bar' }]),
+          {
+            mysql:
+              "replace into `users` (`email`, `name`) values (NULL, 'taylor'), ('bar', NULL)",
+          },
+          mysqlClientWithNullAsDefault
+        );
+      });
+    });
+  });
 });
