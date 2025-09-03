b5fef48f1785763d9b7786d2b489bba20c4cb994
Reece Daniels <rubengmurray@gmail.com>
2025-09-03 20:29:42 +0100

    add new test case for previously failing \? check

--- test/integration2/dialects/postgres.test.js
+++ test/integration2/dialects/postgres.test.js
@@ -1,5 +1,9 @@
 const knex = require('../../../lib');
 
+// TEST CASES NEEDED FOR
+// const connectionA = 'postgres://myuser:myuserpassword@/postgres_knex_db?host=/path/to/unix/socket'
+// const connectionB = 'postgres://myuser:myuserpassword@0.0.0.0:54310/postgres_knex_db?host=/path/to/unix/socket'
+
 describe.only('Postgres dialect', () => {
   describe('Connection configuration', () => {
     describe('Postgres', () => {
@@ -15,14 +19,34 @@ describe.only('Postgres dialect', () => {
             database: 'postgres_knex_db',
           },
         });
+
+        // knex2 = knex({
+        //   client: 'pg',
+        //   connection: connectionA,
+        // });
+
+        // knex2 = knex({
+        //   client: 'pg',
+        //   connection: connectionB,
+        // });
       });
 
       // As it stands, this throws an error
-      xit('Should correctly interpret string literal in JSONB raw SQL', async () => {
+      it.only('Should correctly interpret string literal in JSONB raw SQL', async () => {
         const binding = 'bar_bind';
         const k = knex2.select('*')
           .from('users')
           .whereRaw('json_key.json_value @\\? \'$.*.bar \\? (@ == "?")\'', binding);
+
+        await k;
+      });
+
+      // https://github.com/knex/knex/issues/5189
+      // This now passes
+      it('Should correctly interpret ? in where clause as JSONB query clause', async () => {
+        const k = knex2.select('*')
+          .from('users')
+          .where('json_key', '\\?', 'json_value')
 
         await k;
       });
