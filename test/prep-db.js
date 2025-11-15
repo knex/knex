615d4c58e74583d951d5125f2502a87f71989a96
Kris Reeves <github.com@masquerader.com>
2025-11-15 16:15:40 +0000

    Use a global root "before" hook instead of a fake "it" block
    I noticed that crdb tests still failed with this problem when
    using describe.only to target tests. I had previously tried
    a "before" hook inside a "describe" block, but it turns out
    just a raw "before" at the root does work both for running
    all tests and for running selected tests.

--- test/prep-db.js
+++ test/prep-db.js
@@ -5,24 +5,24 @@ const {
 const { isCockroachDB } = require('./util/db-helpers');
 const { expect } = require('chai');
 
-describe('Pre-test setup', () => {
-  // Out of the box, the CockroachDB docker container does not have the database that all
-  // the integration tests use. The code doesn't create it, so all the crdb tests fail.
-  // I've extracted this logic to a separate file so that it puts the requirements in
-  // place for both the "integration" and the "integration2" workflows
-  it('Prepare DB for integration tests', async () => {
-    const allDbs = getAllDbs();
-    for (const db of allDbs) {
-      const knex = getKnexForDb(db);
-      if (!isCockroachDB(knex)) continue;
+async function prepCRDB(knex) {
+  const dbname = 'test';
+  const configured = knex.client.connectionSettings.database;
+  expect(configured).eq(dbname);
+  if (configured === dbname) {
+    await knex.raw(`DROP DATABASE IF EXISTS "${dbname}"`);
+    await knex.raw(`CREATE DATABASE "${dbname}"`);
+  }
+}
 
-      const dbname = 'test';
-      const configured = knex.client.connectionSettings.database;
-      expect(configured).eq(dbname);
-      if (configured === dbname) {
-        await knex.raw(`DROP DATABASE IF EXISTS "${dbname}"`);
-        await knex.raw(`CREATE DATABASE "${dbname}"`);
-      }
-    }
-  });
+// Out of the box, the CockroachDB docker container does not have the database that all
+// the integration tests use. The code doesn't create it, so all the crdb tests fail.
+// I've extracted this logic to a separate file so that it puts the requirements in
+// place for both the "integration" and the "integration2" workflows
+before(async () => {
+  for (const db of getAllDbs()) {
+    const knex = getKnexForDb(db);
+
+    if (isCockroachDB(knex)) await prepCRDB(knex);
+  }
 });
