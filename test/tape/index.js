d5f8ab6011da68b9afc89c39939b022c7a35021c
myndzi <github.com@masquerader.com>
2025-12-08 07:10:43 +0000

    Clean up promise handling
    Fail at the first prepDB failure, instead of synchronously launching
    a bunch of dangling promises. We still crash out with an uncaught
    rejection if one fails, but the asynchronous flow is much clearer
    this way, and we don't run along trying to register and possibly
    kick off more tests after encountering a failure

--- test/tape/index.js
+++ test/tape/index.js
@@ -15,20 +15,22 @@ require('./pool');
 require('./knex');
 require('./invalid-db-setup')(knexfile);
 
-Object.keys(knexfile).forEach(async function (key) {
-  const knex = makeKnex(knexfile[key]);
+(async () => {
+  for (const key of Object.keys(knexfile)) {
+    const knex = makeKnex(knexfile[key]);
 
-  await prepDB(knex);
+    await prepDB(knex);
 
-  require('./transactions')(knex);
-  require('./stream')(knex);
-  require('./crossdb-compatibility')(knex);
+    require('./transactions')(knex);
+    require('./stream')(knex);
+    require('./crossdb-compatibility')(knex);
 
-  // Tear down the knex connection
-  tape(knex.client.driverName + ' - transactions: after', function (t) {
-    knex.destroy(function () {
-      t.pass('Knex client destroyed');
-      t.end();
+    // Tear down the knex connection
+    tape(knex.client.driverName + ' - transactions: after', function (t) {
+      knex.destroy(function () {
+        t.pass('Knex client destroyed');
+        t.end();
+      });
     });
-  });
-});
+  }
+})();
