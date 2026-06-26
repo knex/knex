22e080e7940a0c13a87facea274939b0492a2d73
Seandon Mooy <seandon.mooy@gmail.com>
2026-06-26 07:36:05 -0700

    micro-optimization in wrappingFormatter (#6456)

--- test/unit/query/formatter.js
+++ test/unit/query/formatter.js
@@ -1,5 +1,8 @@
 const { expect } = require('chai');
 const Formatter = require('../../../lib/formatter');
+const {
+  getAliasSeparatorIndex,
+} = require('../../../lib/formatter/wrappingFormatter');
 const Client = require('../../../lib/client');
 
 describe('formatter', () => {
@@ -27,5 +30,24 @@ describe('formatter', () => {
   it('correctly handles empty array', () => {
     const columns = formatter.columnize([]);
     expect(columns).to.equal('');
+  });
+
+  it('correctly handles dotted identifiers with case-insensitive aliases', () => {
+    const wrapped = formatter.wrap('schema.table AS alias');
+    expect(wrapped).to.equal('"schema"."table" as "alias"');
+  });
+
+  describe('getAliasSeparatorIndex', () => {
+    it('finds aliases case-insensitively', () => {
+      expect(getAliasSeparatorIndex('foo as bar')).to.equal(3);
+      expect(getAliasSeparatorIndex('foo AS bar')).to.equal(3);
+      expect(getAliasSeparatorIndex('foo aS bar')).to.equal(3);
+    });
+
+    it('only treats as as an alias when surrounded by spaces', () => {
+      expect(getAliasSeparatorIndex('foo asbar')).to.equal(-1);
+      expect(getAliasSeparatorIndex('fooas bar')).to.equal(-1);
+      expect(getAliasSeparatorIndex('foo As.bar')).to.equal(-1);
+    });
   });
 });
